696e4a9c8079e644da5a5b6dcd435eafb630e83a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-15 23:48:07 -0300

    feat(theme.ts): Cria tema de cores

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    text: "var(--text)",
+    placeholder: "var(--placeholder)",
+    bg: "var(--bg)",
+    primary: "var(--primary)",
+    input: "var(--input)",
+    inputText: "var(--input-text)",
+    secondary: "var(--secondary)",
+    accent: "var(--accent)",
+  },
+};

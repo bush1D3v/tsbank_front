fef45bf21d586fe30c8c77b2b6d533baf3212dc4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-16 23:37:12 -0300

    update(App.tsx): Adicionando responsividade basica a interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import tsbankLogo from "./assets/tsbank-no-bg.svg";
 
 export default function App() {
   return (
-    <main className="flex flex-col items-center gap-14 justify-center h-[100vh]">
-      <a href="https://github.com/bush1D3v/tsbank_api" target="_blank" className="flex content-center w-[60%]">
+    <main className="flex flex-col gap-6 md:gap-8 lg:gap-10 p-4 md:p-8 lg:p-12 items-center justify-center h-[100vh]">
+      <a href="https://github.com/bush1D3v/tsbank_api" target="_blank" className="">
         <img src={tsbankLogo} alt="Tsbank logo" />
       </a>
-      <p className="font-bold text-2xl px-80 text-center">
+      <p className="text-xl font-bold text-justify md:text-2xl lg:text-3xl lg:text-center">
         Olá! Ficamos felizes que você esteja curioso para conhecer o projeto TSBank! 😄 <br />
         Enquanto a sua primeira página ainda não é criada, sinta-se a vontade para clicar na imagem, você será redirecionado ao repositório do nosso servidor! 🌐
       </p>

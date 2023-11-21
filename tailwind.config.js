d89d7311a18478305686781955324cff213f8bcc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-21 18:51:24 -0300

    build(tailwind.config.js): Adiciona variaveis de cor ao tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}" ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         text: "var(--text)",
+        black: "var(--black)",
         background: "var(--bg)",
         primary: "var(--primary)",
         input: "var(--input)",
         secondary: "var(--secondary)",
         accent: "var(--accent)",
+        error: "var(--error)",
       },
     },
   },

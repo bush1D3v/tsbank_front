9563f05628e2d2496bd02df522e4c01a37a5d237
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-21 19:24:57 -0300

    feat(loginSubmit.ts): Cria funcao de login

---
+++ src/components/FormAuth/components/functions/loginSubmit.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { API_URL_BASE, LOGIN_ENDPOINT } from "../../../../utils";
+import { type loginProps } from "../schemas";
+import { type User } from "../../../../types";
+import { handleError } from "../../../../functions";
+
+interface loginResponseProps {
+  success: boolean;
+  message: string;
+  user?: User;
+}
+
+export default async function handleLoginSubmit(
+  data: loginProps,
+): Promise<loginResponseProps> {
+  const { userData } = data;
+
+  try {
+    const response = await axios.post(`${API_URL_BASE}${LOGIN_ENDPOINT}`, {
+      email: userData.email,
+      password: userData.password,
+    });
+
+    console.log(response);
+
+    if (response.status === 200) {
+      return {
+        success: true,
+        message: "Login completed!",
+        user: response.data,
+      };
+    } else {
+      return {
+        success: false,
+        message: "An error occurred while logging in.",
+      };
+    }
+
+  } catch (error) {
+    return handleError(error);
+  }
+}

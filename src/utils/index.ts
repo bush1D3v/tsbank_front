cfa14276c21a13a5d52200090a1703a789eb814b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-21 19:21:07 -0300

    feat(index.ts): Adiciona index exportador de funcoes utilitarias

---
+++ src/utils/index.ts
@@ -0,0 +1,22 @@
+import {
+  HOME,
+  LOGIN,
+  REGISTER,
+  ROOT
+} from "./routePaths";
+
+import {
+  API_URL_BASE,
+  LOGIN_ENDPOINT,
+  REGISTER_ENDPOINT
+} from "./api";
+
+export {
+  HOME,
+  LOGIN,
+  REGISTER,
+  ROOT,
+  API_URL_BASE,
+  LOGIN_ENDPOINT,
+  REGISTER_ENDPOINT
+};

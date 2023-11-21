59925646c0d913d0be9b0e4f99be5b85878ebfe9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-21 19:26:35 -0300

    feat(Button.ts): Cria componente estilizado de butão

---
+++ src/components/FormAuth/components/styleds/Button.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: var(--secondary);
+  font-weight: 700;
+  padding: 1rem 0;
+  min-width: 100%;
+  border-radius: 10px;
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+  transition: all 400ms;
+
+  &:hover {
+    color: var(--accent);
+  }
+
+  & {
+    @media (min-width: 768px) {
+      width: 80%;
+    }
+
+    @media (min-width: 1024px) {
+      width: 50%;
+    }
+
+    @media (min-width: 1280px) {
+      width: 90%;
+    }
+
+    @media (min-width: 1536px) {
+      width: 50%;
+    }
+  }
+`;

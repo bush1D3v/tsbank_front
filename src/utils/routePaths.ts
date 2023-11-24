a55282cba8d07bbf7b201db4d3d3e007f3890235
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-23 23:44:56 -0300

    update(routePath.ts): Adiciona rotas de perfil e sobre

--- src/utils/routePaths.ts
+++ src/utils/routePaths.ts
@@ -2,3 +2,5 @@ export const ROOT: string = "/";
 export const HOME: string = "/home";
 export const LOGIN: string = "/login";
 export const REGISTER: string = "/register";
+export const PROFILE: string = "/profile";
+export const ABOUT: string = "/about";

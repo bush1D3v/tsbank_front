26067fc4569bc989d313eee6a6a8b70b2b7647cc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-23 23:46:57 -0300

    update(index.ts): Atualiza exportacao de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
 import ErrorPage from "./ErrorPage";
 import Form from "./FormAuth";
 import Modal from "./Modal";
+import Hero from "./Hero";
+import Layout from "./Layout";
 
 export {
   ErrorPage,
   Form,
-  Modal
+  Modal,
+  Hero,
+  Layout
 };

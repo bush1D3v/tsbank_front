70de02a51e2b4bf32e862b5e0796a3f8b787c934
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-11-23 23:43:35 -0300

    feat(Logo.tsx): Cria componente da logo para o header

--- src/components/Header/components/Logo.tsx
+++ src/components/Header/components/Logo.tsx
@@ -1,18 +1,12 @@
 import { type ReactElement } from "react";
 import { BsBank } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
 import { HOME } from "../../../utils";
+import { handleLinkClick } from "../../../functions";
 
 export default function Logo(): ReactElement {
-  const navigate = useNavigate();
-
-  const handleLogoClick = () => {
-    navigate(HOME);
-  };
-
   return (
     <a
-      onClick={handleLogoClick}
+      onClick={handleLinkClick(HOME)}
       className="bank h-100% cursor-pointer text-5xl md:text-7xl lg:text-6xl hover:text-darkBlue
       transition-all delay-75 relative">
       <BsBank />

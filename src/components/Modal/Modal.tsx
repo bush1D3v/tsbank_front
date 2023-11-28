f2ef8080085ca4d56ec58857748cfecf84714aeb
NSJvictor <victornavarro@nasajon.com.br>
2023-11-28 14:33:26 -0300

    update(Modal.tsx):  Atualiza estilizacao do componente

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -20,7 +20,7 @@ export default function Modal({
       <div className="fixed top-0 bottom-0 left-0 right-0 bg-[#00000090]">
         <div
           className="fixed top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]
-          bg-background text-background p-8 rounded-3xl w-11/12 md:w-[52.5%] lg:w-2/5 xl:w-[30%]
+          bg-blueBase p-8 rounded-3xl w-11/12 md:w-[52.5%] lg:w-2/5 xl:w-[30%]
           h-3/6 md:h-[35%] lg:h-2/5 flex flex-col justify-evenly
         "
         >

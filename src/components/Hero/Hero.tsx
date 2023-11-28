8f368be8cd2b82bdf0d90a1878c72176dce3c15c
NSJvictor <victornavarro@nasajon.com.br>
2023-11-28 14:29:44 -0300

    update(Hero.tsx): Atualiza estilizacao do componente

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,10 +4,10 @@ import { type ReactElement } from "react";
 export default function Hero(): ReactElement {
   return (
     <section className="flex flex-col items-center gap-14 justify-center">
-      <a href="https://github.com/bush1D3v/tsbank_api" target="_blank" className="flex content-center w-[60%]">
+      <a href="https://github.com/bush1D3v/tsbank_api" target="_blank" className="flex content-center w-[90%] md:w-[80%] lg:w-[75%] xl:w-[70%]">
         <img src={tsbankLogo} alt="Tsbank logo" />
       </a>
-      <p className="font-bold text-2xl text-justify md:text-center px-2 md:px-4 xl:px-80">
+      <p className="font-bold text-2xl text-justify md:text-center px-2 md:px-4 xl:px-50">
         Olá! Ficamos felizes que você esteja curioso para conhecer o projeto TSBank! 😄 <br />
         Enquanto a sua primeira página ainda não é criada, sinta-se a vontade para clicar na imagem, você será redirecionado ao repositório do nosso servidor! 🌐
       </p>

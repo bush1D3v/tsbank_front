8ce2b1aa95b036e7c0c2ab3c42242ad484a2292d
NSJvictor <victornavarro@nasajon.com.br>
2023-11-28 14:36:06 -0300

    update(index.ts): Atualiza exportacao de paginas

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,11 @@
 import Register from "./Register.tsx";
 import Home from "./Home";
+import About from "./About";
+import Profile from "./Profile";
 
 export {
   Register,
-  Home
+  Home,
+  About,
+  Profile
 };

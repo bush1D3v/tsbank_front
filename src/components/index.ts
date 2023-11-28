2222436894a029274647ea0ca96f34545e4ae27e
NSJvictor <victornavarro@nasajon.com.br>
2023-11-28 14:36:12 -0300

    update(index.ts): Atualiza exportacao de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,15 @@ import Form from "./FormAuth";
 import Modal from "./Modal";
 import Hero from "./Hero";
 import Layout from "./Layout";
+import AboutProject from "./AboutProject";
+import ProfilePage from "./ProfilePage";
 
 export {
   ErrorPage,
   Form,
   Modal,
   Hero,
-  Layout
+  Layout,
+  AboutProject,
+  ProfilePage
 };

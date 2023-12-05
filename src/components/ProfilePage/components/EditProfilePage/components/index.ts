0302255620f0c18c67af62ad0c838628ad70c87f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 23:03:08 -0300

    feat(index.ts): Adiciona index exportador de componentes do EditProfilePage

---
+++ src/components/ProfilePage/components/EditProfilePage/components/index.ts
@@ -0,0 +1,15 @@
+import EditProfilePage from "./EditProfilePage.tsx";
+import EditAllAccInfo from "./EditAllAccInfo";
+import DeleteAcc from "./DeleteAcc";
+import ChangeEmail from "./ChangeEmail";
+import ChangePassword from "./ChangePassword";
+import ChangePhone from "./ChangePhone";
+
+export {
+  EditProfilePage,
+  EditAllAccInfo,
+  DeleteAcc,
+  ChangeEmail,
+  ChangePassword,
+  ChangePhone
+};

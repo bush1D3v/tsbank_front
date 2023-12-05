1782def7c84c8fe036e341f47cd723b460feae0a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 23:03:19 -0300

    feat(index.ts): Adiciona index exportador de funcoes do EditProfilePage

---
+++ src/components/ProfilePage/components/EditProfilePage/components/functions/index.ts
@@ -0,0 +1,13 @@
+import changeEmailSubmit from "./changeEmailSubmit";
+import changePasswordSubmit from "./changePasswordSubmit";
+import changePhoneSubmit from "./changePhoneSubmit";
+import editAllAccInfSubmit from "./editAllAccInfoSubmit";
+import deleteAccSubmit from "./deleteAccSubmit";
+
+export {
+  changeEmailSubmit,
+  changePasswordSubmit,
+  changePhoneSubmit,
+  editAllAccInfSubmit,
+  deleteAccSubmit
+};

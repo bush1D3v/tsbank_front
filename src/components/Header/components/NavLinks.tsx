927c46ddc793e76791a12e166d390c7d0174976f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 22:35:58 -0300

    update(NavLinks.tsx): Atualiza componentizacao da area de navegacao

--- src/components/Header/components/NavLinks.tsx
+++ src/components/Header/components/NavLinks.tsx
@@ -1,13 +1,28 @@
 import { type ReactElement } from "react";
 import { HOME, ABOUT } from "../../../utils";
-import LinkButton from "../../LinkButton";
+import AnchorLink from "../../AnchorLink";
+import { handleLinkClick } from "../../../functions";
 
 export default function NavLinks(): ReactElement {
   return (
     <nav>
       <ul className="flex gap-6 font-bold">
-        <LinkButton to={HOME} text="HOME" />
-        <LinkButton to={ABOUT} text="ABOUT" />
+        <li className="flex">
+          <AnchorLink
+            func={handleLinkClick(HOME)}
+            param={HOME}
+            text="HOME"
+            buttonBg="bg-blueBase hover:bg-transparent"
+          />
+        </li>
+        <li className="flex">
+          <AnchorLink
+            func={handleLinkClick(ABOUT)}
+            param={ABOUT}
+            text="ABOUT"
+            buttonBg="bg-blueBase hover:bg-transparent"
+          />
+        </li>
       </ul>
     </nav>
   );

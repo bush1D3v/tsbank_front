0ca465fe89157cc404991efc13f02bc5099f5093
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 23:02:30 -0300

    feat(index.ts): Adiciona index exportador de componentes do ProfilePage

---
+++ src/components/ProfilePage/components/index.ts
@@ -0,0 +1,9 @@
+import TableRow from "./TableRow";
+import MainEditProfile from "./EditProfilePage";
+import ProfilePage from "./ProfilePage";
+
+export {
+  TableRow,
+  MainEditProfile,
+  ProfilePage
+};

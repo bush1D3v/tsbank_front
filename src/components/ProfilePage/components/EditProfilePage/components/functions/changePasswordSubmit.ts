76ad077442850b4c12d2fea8cc243507b5f6d076
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 22:54:46 -0300

    feat(changePasswordSubmit.ts): Cria funcao para envio de dados para a requisicao de atualizar senha

---
+++ src/components/ProfilePage/components/EditProfilePage/components/functions/changePasswordSubmit.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { changePasswordProps } from "../schemas";
+import { handleError, jsonUserParser } from "../../../../../../functions";
+import { API_URL_BASE, UPDATE_PASSWORD_ENDPOINT } from "../../../../../../utils";
+
+interface changePasswordResponseProps {
+  success: boolean;
+  message: string;
+}
+
+export default async function changePasswordSubmit(
+  data: changePasswordProps
+): Promise<changePasswordResponseProps> {
+  const { userData } = data;
+
+  const { token } = jsonUserParser(localStorage.getItem("userInfo"));
+
+  try {
+    const response = await axios.patch(`${API_URL_BASE}${UPDATE_PASSWORD_ENDPOINT}`,
+      {
+        new_password: userData.new_password,
+        password: userData.password,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (response.status === 201) {
+      return {
+        success: true,
+        message: response.data
+      };
+    } else {
+      return {
+        success: false,
+        message: "An error occurred while password exchange.",
+      };
+    }
+  } catch (error) {
+    return handleError(error);
+  }
+}

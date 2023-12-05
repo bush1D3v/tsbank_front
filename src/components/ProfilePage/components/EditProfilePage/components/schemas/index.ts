0782ad20506c16c569675a94e6c6fbcd1ca3ec92
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 23:03:29 -0300

    feat(index.ts): Adiciona index exportador de esquemas do EditProfilePage

---
+++ src/components/ProfilePage/components/EditProfilePage/components/schemas/index.ts
@@ -0,0 +1,33 @@
+import {
+  changeEmailSchema,
+  changeEmailProps
+} from "./changeEmailSchema";
+import {
+  changePasswordSchema,
+  changePasswordProps
+} from "./changePasswordSchema";
+import {
+  changePhoneProps,
+  changePhoneSchema
+} from "./changePhoneSchema";
+import {
+  deleteAccSchema,
+  deleteAccProps
+} from "./deleteAccSchema";
+import {
+  editAllAccInfoSchema,
+  editAllAccInfoProps
+} from "./editAllAccInfoSchema";
+
+export {
+  changeEmailSchema,
+  type changeEmailProps,
+  changePasswordSchema,
+  type changePasswordProps,
+  changePhoneSchema,
+  type changePhoneProps,
+  editAllAccInfoSchema,
+  type editAllAccInfoProps,
+  deleteAccSchema,
+  type deleteAccProps,
+};

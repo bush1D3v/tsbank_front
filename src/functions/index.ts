b32dce5232a24b9773bf1e9297571f6f141ff745
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 23:01:02 -0300

    update(index.ts): Atualiza exportacao de funcoes

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,10 +2,14 @@ import handleError from "./handleError";
 import handleLinkClick from "./handleLinkClick";
 import jsonUserParser from "./jsonUserParser";
 import logoutClick from "./logoutClick";
+import eventClick from "./eventClick";
+import localStorageStringfy from "./localStorageStringfy";
 
 export {
   handleError,
   handleLinkClick,
   jsonUserParser,
-  logoutClick
+  logoutClick,
+  eventClick,
+  localStorageStringfy
 };

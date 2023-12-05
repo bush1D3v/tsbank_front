8427d86054df1a2d1c22576a66faa2ed53a97c28
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-04 22:35:28 -0300

    update(Header.tsx): Atualiza cor do header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import { type ReactElement } from "react";
 import { Logo, NavLinks, Profile } from "./components";
 
-export function Header(): ReactElement {
+export default function Header(): ReactElement {
   return (
-    <header className="bg-saturedBlue flex h-[9dvh] items-center justify-between py-2 px-4">
+    <header className="bg-saturatedBlue flex h-[9dvh] items-center justify-between py-2 px-4">
       <Logo />
       <NavLinks />
       <Profile />

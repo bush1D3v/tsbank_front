52b5906d56e2d17c0d2a40c19e11dfe11686e7ed
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:54:08 -0300

    feat(index.ts): Adiciona index exportador de esquemas do componente card dentro do componente home

---
+++ src/components/Home/components/Card/components/schemas/index.ts
@@ -0,0 +1,15 @@
+import { cardCreateProps, cardCreateSchema } from "./cardCreateSchema";
+import { cardUpdateProps, cardUpdateSchema } from "./cardUpdateSchema";
+import { cardTransactionProps, cardTransactionSchema } from "./cardTransactionSchema";
+import { creditPaymentProps, creditPaymentSchema } from "./creditPaymentSchema";
+
+export {
+  cardCreateSchema,
+  type cardCreateProps,
+  cardUpdateSchema,
+  type cardUpdateProps,
+  cardTransactionSchema,
+  type cardTransactionProps,
+  creditPaymentSchema,
+  type creditPaymentProps
+};

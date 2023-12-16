454c98a3441a02a42c6d4a1bef35b8307ff683a9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:03:51 -0300

    feat(withdrawSchema.ts): Cria esquema para validar dados de saque

---
+++ src/components/Home/components/schemas/withdrawSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const withdrawSchema = z.object({
+  userData: z.object({
+    value: z.string()
+      .min(1, { message: "The value cannot be equal to or less than 0" }),
+    password: z.string()
+      .min(8, { message: "Please enter a valid password" }),
+  }),
+});
+
+export type withdrawProps = z.infer<typeof withdrawSchema>;

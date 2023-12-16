333cfd4550de343fee5cc2b685efa8310fc5373c
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-16 20:05:51 -0300

    fix(Footer.tsx): fix misspell "Make" to "Made"

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer(): ReactElement {
     <footer className="text-sm bg-saturatedBlue flex h-[9dvh] justify-between py-2 px-4 relative">
       <span className="md:text-xl my-auto">TSBank <br /> Corporation &copy;</span>
       <p className="absolute left-20 right-20 md:static md:mr-[10dvw] lg:mr-[7dvw] xl:mr-[4dvw] md:text-xl md:my-auto text-center">
-        Make with 💖 <br /> by <a
+        Made with 💖 <br /> by <a
           className="font-bold underline cursor-pointer hover:text-darkBlue transition-all"
           href="https://linktr.ee/bush1D3v"
           target="_blank"

ac879badf09910df97cccdd488228a3a4ec94d87
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:53:58 -0300

    feat(index.ts): Adiciona index exportador de funcoes do componente card dentro do componente home

---
+++ src/components/Home/components/Card/components/functions/index.ts
@@ -0,0 +1,15 @@
+import cardCreateSubmit from "./cardCreateSubmit";
+import cardTransactionSubmit from "./cardTransactionSubmit";
+import cardUpdateSubmit from "./cardUpdateSubmit";
+import creditPaymentSubmit from "./creditPaymentSubmit";
+import { cardDetailSubmit, CardDetailResponseProps, CardData } from "./cardDetailSubmit";
+
+export {
+  cardCreateSubmit,
+  cardUpdateSubmit,
+  cardTransactionSubmit,
+  creditPaymentSubmit,
+  cardDetailSubmit,
+  type CardDetailResponseProps,
+  type CardData
+};

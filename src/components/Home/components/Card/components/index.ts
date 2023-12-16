d08b0d2538cb0dae1238ad9b442c07cc47521d24
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:42:11 -0300

    feat(index.ts): Adiciona index exportador de componentes do componente card

---
+++ src/components/Home/components/Card/components/index.ts
@@ -0,0 +1,13 @@
+import CardDetail from "./CardDetail";
+import CreditPayment from "./CreditPayment";
+import CardTransaction from "./CardTransaction";
+import CardUpdate from "./CardUpdate";
+import CardCreate from "./CardCreate";
+
+export {
+  CardUpdate,
+  CardTransaction,
+  CreditPayment,
+  CardDetail,
+  CardCreate
+};

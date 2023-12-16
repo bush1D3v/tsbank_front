1203bd3f5ec2d0f817a47c1e979264887ea0ea09
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 18:20:17 -0300

    update(Home.tsx): Atualiza tratamento de dados da requisicao e cacheamento de dados

--- src/components/Home/components/Home.tsx
+++ src/components/Home/components/Home.tsx
@@ -46,7 +46,7 @@ export default function Home(): ReactElement {
       setIsLoading(false);
     } else if (errorData) {
       setHistoryData(JSON.parse(errorData));
-      fetchData();
+      setIsLoading(false);
     } else {
       fetchData();
     }

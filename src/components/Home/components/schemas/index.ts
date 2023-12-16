3f928fe1aaa0167170b733ddb9c48a98dbc87397
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:41:32 -0300

    feat(index.ts): Adiciona index exportador de esquemas da home

---
+++ src/components/Home/components/schemas/index.ts
@@ -0,0 +1,12 @@
+import { depositProps, depositSchema } from "./depositSchema";
+import { pixProps, pixSchema } from "./pixSchema";
+import { withdrawProps, withdrawSchema } from "./withdrawSchema";
+
+export {
+  pixSchema,
+  type pixProps,
+  depositSchema,
+  type depositProps,
+  withdrawSchema,
+  type withdrawProps
+};

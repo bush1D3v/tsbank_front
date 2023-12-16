6479d7138c4cef5f568f0fb704c361ba659c8aef
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:48:08 -0300

    feat(index.ts): Adiciona index exportador de componentes do componente Home

---
+++ src/components/Home/components/index.ts
@@ -0,0 +1,13 @@
+import MainCard from "./Card";
+import Deposit from "./Deposit";
+import Home from "./Home";
+import Pix from "./Pix";
+import Withdraw from "./Withdraw";
+
+export {
+  Home,
+  MainCard,
+  Deposit,
+  Pix,
+  Withdraw
+};

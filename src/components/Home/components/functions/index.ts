039106b97a618ac8e7325a8ce2fe17ae39a0685a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:41:45 -0300

    feat(index.ts): Adiciona index exportador de funcoes da home

---
+++ src/components/Home/components/functions/index.ts
@@ -0,0 +1,17 @@
+import depositSubmit from "./depositSubmit";
+import pixSubmit from "./pixSubmit";
+import withdrawSubmit from "./withdrawSubmit";
+import {
+  getHistory,
+  type HistoryData,
+  type HistoryResponseProps
+} from "./getHistory";
+
+export {
+  depositSubmit,
+  pixSubmit,
+  withdrawSubmit,
+  getHistory,
+  type HistoryData,
+  type HistoryResponseProps
+};

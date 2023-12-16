3d1c35b06f9c8152becb1490bf06a926814729b4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:38:23 -0300

    feat(pixSchema.ts): Cria esquema validador de dados da requisicao de pix

---
+++ src/components/Home/components/schemas/pixSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const pixSchema = z.object({
+  userData: z.object({
+    password: z.string()
+      .min(8, { message: "Please enter a valid password" }),
+    value: z.string()
+      .min(1, { message: "The value cannot be equal to or less than 0" }),
+    cpf: z.string()
+      .min(11, { message: "Please enter a valid CPF" })
+      .max(11, { message: "Please enter a valid CPF" })
+  }),
+});
+
+export type pixProps = z.infer<typeof pixSchema>;

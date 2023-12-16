146f4fa98716cce66098f1856240aa90c59865df
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:38:49 -0300

    feat(depositSchema.ts): Cria esquema validador de dados da requisicao de deposito

---
+++ src/components/Home/components/schemas/depositSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const depositSchema = z.object({
+  userData: z.object({
+    value: z.string()
+      .min(1, { message: "The value cannot be equal to or less than 0" }),
+    email: z.string()
+      .min(1, { message: "This field has to be filled" })
+      .email("This is not a valid email"),
+    password: z.string()
+      .min(8, { message: "Please enter a valid password" }),
+  }),
+});
+
+export type depositProps = z.infer<typeof depositSchema>;

49f9a73e8c47c5686877185f155a73749628b836
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 17:40:08 -0300

    feat(cardUpdateSchema.ts): Cria esquema validador de dados da requisicao de atualizacao de senha do cartao

---
+++ src/components/Home/components/Card/components/schemas/cardUpdateSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const cardUpdateSchema = z.object({
+  userData: z.object({
+    new_password: z.string()
+      .min(4, { message: "Password have at least 4 characters" })
+      .max(6, { message: "Passwords limited to 6 characters" }),
+    password: z.string()
+      .min(4, { message: "Password have at least 4 characters" })
+      .max(6, { message: "Passwords limited to 6 characters" }),
+    card_type: z.string()
+      .min(5, { message: "Card type is 'credit' or 'debit'" })
+      .max(6, { message: "Card type is 'credit' or 'debit'" }),
+  }),
+});
+
+export type cardUpdateProps = z.infer<typeof cardUpdateSchema>;

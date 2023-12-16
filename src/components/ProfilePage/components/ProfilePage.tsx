f0652bd87e000e12e6af30210d2412215d50e3f5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-16 01:07:00 -0300

    update(ProfilePage.tsx): Atualiza dados exibidos no perfil

--- src/components/ProfilePage/components/ProfilePage.tsx
+++ src/components/ProfilePage/components/ProfilePage.tsx
@@ -10,18 +10,16 @@ import {
 import AnchorLink from "../../AnchorLink";
 
 export default function ProfilePage(): ReactElement {
-  const stringUserData = localStorage.getItem("userInfo");
+  const stringUserData = sessionStorage.getItem("userInfo");
 
   const jsonUserData = jsonUserParser(stringUserData);
 
   return (
     <div className="flex justify-center items-center h-[82dvh] flex-col gap-10 md:gap-14 lg:gap-20 p-2">
-      <table className="h-content flex w-full max-w-sm flex-col justify-center gap-2 rounded-md border-2 border-lightGray p-1 md:scale-110 lg:scale-125">
+      <table className="h-content flex w-full max-w-sm flex-col justify-center gap-2 rounded-md border-2 border-lightGray p-2 md:scale-125 lg:scale-150">
         <thead className="flex justify-center pb-6">
           <CgProfile className="h-[10dvh] w-[10dvh]" />
         </thead>
-        <TableRow label="User:" value={jsonUserData.user.name} />
-        <TableRow label="Balance:" value={jsonUserData.user.balance} />
         <TableRow label="Email:" value={jsonUserData.user.email} />
         <TableRow label="Phone:" value={jsonUserData.user.phone} />
         <TableRow label="Cpf:" value={jsonUserData.user.cpf} />

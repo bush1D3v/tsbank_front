0d85e36b8ec8070b9f69def6b7eda88b5bb3dfd4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-18 00:46:17 -0300

    update(index.ts): Atualiza exportacao de componentes do componente Home

--- src/components/Home/components/index.ts
+++ src/components/Home/components/index.ts
@@ -3,11 +3,13 @@ import Deposit from "./Deposit";
 import Home from "./Home";
 import Pix from "./Pix";
 import Withdraw from "./Withdraw";
+import TransactionDetail from "./TransactionDetail";
 
 export {
   Home,
   MainCard,
   Deposit,
   Pix,
-  Withdraw
+  Withdraw,
+  TransactionDetail
 };

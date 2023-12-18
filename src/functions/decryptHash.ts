a06cb0df3f7eeed3247bb44f9e979af821a64749
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-18 00:44:11 -0300

    feat(decryptHash.ts): Cria funcao para desincriptar hash do id passado na url

---
+++ src/functions/decryptHash.ts
@@ -0,0 +1,6 @@
+import CryptoJS from "crypto-js";
+import key from "../crypto";
+
+export default function decryptHash(id: string) {
+  return CryptoJS.AES.decrypt(id, key).toString(CryptoJS.enc.Utf8);
+}

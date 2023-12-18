f1acbaec7b0573f0fcd5d324936f7f44a89a5568
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-18 00:37:21 -0300

    fix(Footer.tsx): Atualiza identacao do texto

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer(): ReactElement {
   return (
     <footer className="text-sm bg-saturatedBlue flex h-[9dvh] justify-between py-2 px-4 relative">
       <span className="md:text-xl my-auto">TSBank <br /> Corporation &copy;</span>
-      <p className="absolute left-20 right-20 md:static md:mr-[10dvw] lg:mr-[7dvw] xl:mr-[4dvw] md:text-xl md:my-auto text-center">
+      <p className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] text-center md:text-xl">
         Made with 💖 <br /> by <a
           className="font-bold underline cursor-pointer hover:text-darkBlue transition-all"
           href="https://linktr.ee/bush1D3v"

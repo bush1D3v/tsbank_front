81f78a909a0b7f61d2503e13d84c7f69596844fc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-07 18:33:07 -0300

    fix(Modal.tsx): Alterando variavel de cor de text para white

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -24,11 +24,11 @@ export default function Modal({
           h-3/6 md:h-[35%] lg:h-2/5 flex flex-col justify-evenly
         "
         >
-          <h2 className="text-4xl mb-2 font-bold text-text">{title}</h2>
-          <p className="text-text mb-3 text-2xl">{description}</p>
+          <h2 className="text-4xl mb-2 font-bold text-white">{title}</h2>
+          <p className="text-white mb-3 text-2xl">{description}</p>
           <button
             type="button"
-            className="text-text px-7 py-2 bg-secondary font-bold rounded-[10px] text-xl hover:text-accent transition-colors"
+            className="text-white px-7 py-2 bg-secondary font-bold rounded-[10px] text-xl hover:text-accent transition-colors"
             onClick={() => {
               setOpen(!isOpen);
             }}

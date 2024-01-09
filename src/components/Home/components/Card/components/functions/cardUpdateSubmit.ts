e43841552631d035cf4dd355a20a619032558810
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:16:38 -0300

    refactor(cardUpdateSubmit.ts): Adicionando variaveis de ambiente ao roteamento

--- src/components/Home/components/Card/components/functions/cardUpdateSubmit.ts
+++ src/components/Home/components/Card/components/functions/cardUpdateSubmit.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 import { type cardUpdateProps } from "../schemas";
 import { handleError, jsonUserParser } from "../../../../../../functions";
-import { API_URL_BASE, CARD_UPDATE_ENDPOINT } from "../../../../../../utils";
 
 interface cardUpdateResponseProps {
   success: boolean;
@@ -16,8 +15,13 @@ export default async function cardUpdateSubmit(
 
   const { token } = jsonUserParser(sessionStorage.getItem("userInfo"));
 
+  const {
+    VITE_REACT_APP_API_BASE_URL,
+    VITE_REACT_APP_CARD_UPDATE_ENDPOINT
+  } = import.meta.env;
+
   try {
-    const response = await axios.patch(`${API_URL_BASE}${CARD_UPDATE_ENDPOINT}`,
+    const response = await axios.patch(`${VITE_REACT_APP_API_BASE_URL}${VITE_REACT_APP_CARD_UPDATE_ENDPOINT}`,
       {
         new_password: userData.new_password,
         password: userData.password,

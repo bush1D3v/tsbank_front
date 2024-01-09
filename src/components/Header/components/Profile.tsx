463d3c1d8b3de968d1c71f4fedb9093fcf19001f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:07:53 -0300

    refactor(Profile.tsx): Adicionando variaveis de ambiente ao roteamento

--- src/components/Header/components/Profile.tsx
+++ src/components/Header/components/Profile.tsx
@@ -1,12 +1,15 @@
 import { type ReactElement } from "react";
 import { CgProfile } from "react-icons/cg";
-import { PROFILE } from "../../../utils";
 import { handleLinkClick } from "../../../functions";
 
 export default function Profile(): ReactElement {
+  const {
+    VITE_REACT_APP_PROFILE
+  } = import.meta.env;
+
   return (
     <a
-      onClick={handleLinkClick(PROFILE)}
+      onClick={handleLinkClick(VITE_REACT_APP_PROFILE)}
       className="bank h-100% cursor-pointer text-5xl md:text-7xl lg:text-6xl hover:text-darkBlue
       transition-all delay-75 relative">
       <CgProfile />

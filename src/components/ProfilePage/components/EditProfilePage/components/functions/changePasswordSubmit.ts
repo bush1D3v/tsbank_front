cae3b3649c0f2255df9ea6efbcc0f77be7fa9495
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:17:25 -0300

    refactor(changePasswordSubmit.ts): Adicionando variaveis de ambiente ao roteamento

--- src/components/ProfilePage/components/EditProfilePage/components/functions/changePasswordSubmit.ts
+++ src/components/ProfilePage/components/EditProfilePage/components/functions/changePasswordSubmit.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { type changePasswordProps } from "../schemas";
 import { handleError, jsonUserParser } from "../../../../../../functions";
-import { API_URL_BASE, UPDATE_PASSWORD_ENDPOINT } from "../../../../../../utils";
 
 interface changePasswordResponseProps {
   success: boolean;
@@ -15,8 +14,13 @@ export default async function changePasswordSubmit(
 
   const { token } = jsonUserParser(sessionStorage.getItem("userInfo"));
 
+  const {
+    VITE_REACT_APP_API_BASE_URL,
+    VITE_REACT_APP_UPDATE_PASSWORD_ENDPOINT
+  } = import.meta.env;
+
   try {
-    const response = await axios.patch(`${API_URL_BASE}${UPDATE_PASSWORD_ENDPOINT}`,
+    const response = await axios.patch(`${VITE_REACT_APP_API_BASE_URL}${VITE_REACT_APP_UPDATE_PASSWORD_ENDPOINT}`,
       {
         new_password: userData.new_password,
         password: userData.password,

0f2da1ceb5bcb0c299c9764aa2f5699d08edc02b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:10:11 -0300

    refactor(privateRoutes.tsx): Adicionando variaveis de ambiente ao roteamento

--- src/helpers/privateRoutes.tsx
+++ src/helpers/privateRoutes.tsx
@@ -1,11 +1,14 @@
 import { type ReactElement, useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-import { LOGIN } from "../utils/routePaths";
-import { UserDataContext } from "../contexts/userData";
+import { UserDataContext } from "../contexts";
 
 export default function PrivateRoutes(): ReactElement {
   const { isUserLoggedIn } = useContext(UserDataContext);
 
-  return isUserLoggedIn ? <Outlet /> : <Navigate to={LOGIN} />;
+  const {
+    VITE_REACT_APP_LOGIN
+  } = import.meta.env;
+
+  return isUserLoggedIn ? <Outlet /> : <Navigate to={VITE_REACT_APP_LOGIN} />;
 }

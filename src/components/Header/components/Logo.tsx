86588866da95295bc95922e7ae89f7073f9db7b5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:07:32 -0300

    refactor(Logo.tsx): Adicionando variaveis de ambiente ao roteamento

--- src/components/Header/components/Logo.tsx
+++ src/components/Header/components/Logo.tsx
@@ -1,12 +1,15 @@
 import { type ReactElement } from "react";
 import { BsBank } from "react-icons/bs";
-import { HOME } from "../../../utils";
 import { handleLinkClick } from "../../../functions";
 
 export default function Logo(): ReactElement {
+  const {
+    VITE_REACT_APP_HOME
+  } = import.meta.env;
+
   return (
     <a
-      onClick={handleLinkClick(HOME)}
+      onClick={handleLinkClick(VITE_REACT_APP_HOME)}
       className="bank h-100% cursor-pointer text-5xl md:text-7xl lg:text-6xl hover:text-darkBlue
       transition-all delay-75 relative">
       <BsBank />

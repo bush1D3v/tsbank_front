cfda514ae2d7f9f816e6e8348ee0591126a54743
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:05:39 -0300

    refactor(Router.tsx): Adicionando variaveis de ambiente ao roteamento

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,39 +6,40 @@ import {
   Layout
 } from "../components";
 import {
-  ROOT,
-  HOME,
-  REGISTER,
-  LOGIN,
-  ABOUT,
-  PROFILE,
-  EDIT,
-  EDIT_ALL,
-  EDIT_EMAIL,
-  EDIT_PASSWORD,
-  EDIT_PHONE,
-  DELETE_USER,
-  PIX,
-  WITHDRAW,
-  DEPOSIT,
-  CARD,
-  CARD_CREATE,
-  CREDIT_PAYMENT,
-  CARD_UPDATE,
-  CARD_TRANSACTION,
-  DETAIL_TRANSACTION
-} from "../utils";
-import {
   Home,
   Register,
   About,
   Profile
 } from "../pages";
 
+const {
+  VITE_REACT_APP_ROOT,
+  VITE_REACT_APP_LOGIN,
+  VITE_REACT_APP_REGISTER,
+  VITE_REACT_APP_HOME,
+  VITE_REACT_APP_WITHDRAW,
+  VITE_REACT_APP_DEPOSIT,
+  VITE_REACT_APP_PIX,
+  VITE_REACT_APP_PROFILE,
+  VITE_REACT_APP_CREDIT_PAYMENT,
+  VITE_REACT_APP_DETAIL_TRANSACTION,
+  VITE_REACT_APP_CARD_UPDATE,
+  VITE_REACT_APP_DELETE_USER,
+  VITE_REACT_APP_UPDATE_PHONE,
+  VITE_REACT_APP_UPDATE_EMAIL,
+  VITE_REACT_APP_ABOUT,
+  VITE_REACT_APP_UPDATE_PASSWORD,
+  VITE_REACT_APP_UPDATE_ALL,
+  VITE_REACT_APP_UPDATE,
+  VITE_REACT_APP_CARD,
+  VITE_REACT_APP_CARD_TRANSACTION,
+  VITE_REACT_APP_CARD_CREATE
+} = import.meta.env;
+
 const router = createBrowserRouter([
   {
-    path: ROOT,
-    element: <Navigate to={LOGIN} replace />,
+    path: VITE_REACT_APP_ROOT,
+    element: <Navigate to={VITE_REACT_APP_LOGIN} replace />,
     errorElement: <ErrorPage />,
   },
   {
@@ -48,91 +49,83 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
           {
-            path: HOME,
+            path: VITE_REACT_APP_HOME,
             element: <Home />
           },
           {
-            path: DETAIL_TRANSACTION,
+            path: VITE_REACT_APP_DETAIL_TRANSACTION,
             element: <Home />
           },
           {
-            path: WITHDRAW,
+            path: VITE_REACT_APP_WITHDRAW,
             element: <Home />
           },
           {
-            path: DEPOSIT,
+            path: VITE_REACT_APP_DEPOSIT,
             element: <Home />
           },
           {
-            path: CARD,
+            path: VITE_REACT_APP_CARD,
             element: <Home />
           },
           {
-            path: CARD_CREATE,
+            path: VITE_REACT_APP_CARD_CREATE,
             element: <Home />
           },
           {
-            path: CREDIT_PAYMENT,
+            path: VITE_REACT_APP_CREDIT_PAYMENT,
             element: <Home />
           },
           {
-            path: CARD_UPDATE,
+            path: VITE_REACT_APP_CARD_UPDATE,
             element: <Home />
           },
           {
-            path: CARD_UPDATE,
+            path: VITE_REACT_APP_CARD_TRANSACTION,
             element: <Home />
           },
           {
-            path: CARD_TRANSACTION,
+            path: VITE_REACT_APP_PIX,
             element: <Home />
           },
           {
-            path: PIX,
-            element: <Home />
-          },
-          {
-            path: ABOUT,
+            path: VITE_REACT_APP_ABOUT,
             element: <About />
           },
           {
-            path: PROFILE,
+            path: VITE_REACT_APP_PROFILE,
             element: <Profile />,
           },
           {
-            path: EDIT,
+            path: VITE_REACT_APP_UPDATE,
             element: <Profile />
           },
           {
-            path: EDIT_ALL,
+            path: VITE_REACT_APP_UPDATE_ALL,
             element: <Profile />
           },
           {
-            path: EDIT_EMAIL,
+            path: VITE_REACT_APP_UPDATE_EMAIL,
             element: <Profile />
           },
           {
-            path: EDIT_PASSWORD,
+            path: VITE_REACT_APP_UPDATE_PASSWORD,
             element: <Profile />
           },
           {
-            path: EDIT_PHONE,
+            path: VITE_REACT_APP_UPDATE_PHONE,
             element: <Profile />
           },
           {
-            path: EDIT_ALL,
-            element: <Profile />
-          },
-          {
-            path: DELETE_USER,
+            path: VITE_REACT_APP_DELETE_USER,
             element: <Profile />
           },
         ]
       },
     ]
   },
-  { path: REGISTER, element: <Register /> },
-  { path: LOGIN, element: <Register /> },
+  { path: VITE_REACT_APP_REGISTER, element: <Register /> },
+  { path: VITE_REACT_APP_LOGIN, element: <Register /> },
 ]);
 
 export default router;

b6827be58fd41e0a8479ec26d945ff555411d70c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:09:30 -0300

    refactor(CardTransaction.tsx): Adicionando variaveis de ambiente ao roteamento

--- src/components/Home/components/Card/components/CardTransaction.tsx
+++ src/components/Home/components/Card/components/CardTransaction.tsx
@@ -3,7 +3,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigate } from "react-router-dom";
 
-import { CARD } from "../../../../../utils";
 import { balanceStringify, jsonUserParser } from "../../../../../functions";
 import { type cardTransactionProps, cardTransactionSchema } from "./schemas";
 import { cardTransactionSubmit } from "./functions";
@@ -14,6 +13,11 @@ export default function CardTransaction(): ReactElement {
   const [ error, setError ] = useState<string>("");
   const [ isLoading, setIsLoading ] = useState<boolean>(false);
   const [ isModalOpen, setIsModalOpen ] = useState<boolean>(false);
+
+  const {
+    VITE_REACT_APP_CARD
+  } = import.meta.env;
+
   const navigate = useNavigate();
 
   const {
@@ -52,7 +56,7 @@ export default function CardTransaction(): ReactElement {
         });
       }
       setIsLoading(false);
-      navigate(CARD);
+      navigate(VITE_REACT_APP_CARD);
     }
   };
 

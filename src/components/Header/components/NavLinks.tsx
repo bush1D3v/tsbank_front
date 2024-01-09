f4db6c1da43200bfb35b53c1ea6f52df450413ab
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:07:43 -0300

    refactor(NavLink.tsx): Adicionando variaveis de ambiente ao roteamento

--- src/components/Header/components/NavLinks.tsx
+++ src/components/Header/components/NavLinks.tsx
@@ -1,24 +1,28 @@
 import { type ReactElement } from "react";
-import { HOME, ABOUT } from "../../../utils";
 import AnchorLink from "../../AnchorLink";
 import { handleLinkClick } from "../../../functions";
 
 export default function NavLinks(): ReactElement {
+  const {
+    VITE_REACT_APP_HOME,
+    VITE_REACT_APP_ABOUT
+  } = import.meta.env;
+
   return (
     <nav>
       <ul className="flex gap-6 font-bold">
         <li className="flex">
           <AnchorLink
-            func={handleLinkClick(HOME)}
-            param={HOME}
+            func={handleLinkClick(VITE_REACT_APP_HOME)}
+            param={VITE_REACT_APP_HOME}
             text="HOME"
             buttonBg="bg-blueBase hover:bg-transparent"
           />
         </li>
         <li className="flex">
           <AnchorLink
-            func={handleLinkClick(ABOUT)}
-            param={ABOUT}
+            func={handleLinkClick(VITE_REACT_APP_ABOUT)}
+            param={VITE_REACT_APP_ABOUT}
             text="ABOUT"
             buttonBg="bg-blueBase hover:bg-transparent"
           />

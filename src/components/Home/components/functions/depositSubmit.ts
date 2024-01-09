47ce71e21ad240e446c20e773ab372039461252c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 18:16:59 -0300

    refactor(depositSubmit.ts): Adicionando variaveis de ambiente ao roteamento

--- src/components/Home/components/functions/depositSubmit.ts
+++ src/components/Home/components/functions/depositSubmit.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 import { depositProps } from "../schemas";
 import { handleError, jsonUserParser } from "../../../../functions";
-import { API_URL_BASE, DEPOSIT_ENDPOINT } from "../../../../utils";
 import { getHistory } from ".";
 
 interface depositResponseProps {
@@ -17,8 +16,13 @@ export default async function depositSubmit(
 
   const { token } = jsonUserParser(sessionStorage.getItem("userInfo"));
 
+  const {
+    VITE_REACT_APP_API_BASE_URL,
+    VITE_REACT_APP_DEPOSIT_ENDPOINT
+  } = import.meta.env;
+
   try {
-    const response = await axios.post(`${API_URL_BASE}${DEPOSIT_ENDPOINT}`,
+    const response = await axios.post(`${VITE_REACT_APP_API_BASE_URL}${VITE_REACT_APP_DEPOSIT_ENDPOINT}`,
       {
         email: userData.email,
         value: userData.value,

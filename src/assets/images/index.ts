3b62175b90fc125cf880d9155973a20528ce98e8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 08:44:21 -0300

    feat(index.ts): Adiciona index exportador de imagens

---
+++ src/assets/images/index.ts
@@ -0,0 +1,15 @@
+import lpDevelopmentTeam from "./development-team-lp.png";
+import lpInterface from "./interface-lp.png";
+import lpWelcome from "./welcome-lp.png";
+import lpResponsive from "./responsive-lp.png";
+import simpleLogo from "./tsbank-logo.svg";
+import completeLogo from "./tsbank-no-bg.png";
+
+export {
+  lpDevelopmentTeam,
+  lpInterface,
+  lpWelcome,
+  lpResponsive,
+  simpleLogo,
+  completeLogo
+};

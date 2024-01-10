a02a95d2e1a6d82e48f9aeadf6e4c258846d998b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-09 22:33:47 -0300

    feat(Skeleton.tsx): Cria componente de Skeleton

---
+++ src/components/AboutProject/components/Skeleton.tsx
@@ -0,0 +1,19 @@
+import { type ReactElement } from "react";
+
+export default function Skeleton(): ReactElement {
+  return (
+    <div className="border p-4 rounded shadow-2xl w-[98dvw] h-[90dvw] md:w-[450px] md:h-[450px] my-5">
+      <div className="relative h-full mb-4 flex justify-center items-center bg-gray-300 animate-pulse">
+        <svg
+          className="w-10 h-10 text-gray-600"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 18"
+        >
+          <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+        </svg>
+      </div>
+    </div>
+  );
+}

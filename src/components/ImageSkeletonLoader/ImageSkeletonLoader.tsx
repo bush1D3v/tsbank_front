ebed0d6d93b3d94809a8c42ab09888022ce94e6a
NSJvictor <victornavarro@nasajon.com.br>
2024-01-10 18:48:14 -0300

    feat(ImageSkeletonLoader.tsx): Cria componente de loader skeleton renderizador de imagem

---
+++ src/components/ImageSkeletonLoader/ImageSkeletonLoader.tsx
@@ -0,0 +1,55 @@
+import {
+  type ReactElement,
+  useState,
+  useEffect
+} from "react";
+import { Skeleton } from "./components";
+
+interface ImageSkeletonLoaderProps {
+  src: string;
+  alt: string;
+  loading: "lazy" | "eager";
+  sessionStorageItem: string;
+}
+
+export default function ImageSkeletonLoader({
+  src,
+  alt,
+  loading,
+  sessionStorageItem
+}: ImageSkeletonLoaderProps): ReactElement {
+  const [ isSkeletonNotLoading, setIsSkeletonNotLoading ] = useState(false);
+  const [ isImageLoading, setIsImageLoading ] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsImageLoading(true);
+  };
+
+  useEffect(() => {
+    const loading = sessionStorage.getItem(sessionStorageItem);
+
+    if (loading !== null) {
+      setIsSkeletonNotLoading(true);
+    } else {
+      const timeout = setTimeout(() => {
+        setIsSkeletonNotLoading(true);
+        sessionStorage.setItem(sessionStorageItem, "true");
+      }, 500);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [ sessionStorageItem ]);
+
+  return (
+    <>
+      {isSkeletonNotLoading ? (
+        <>
+          <img src={src} alt={alt} loading={loading} onLoad={handleImageLoad} />
+          {isImageLoading ? null : <Skeleton />}
+        </>
+      ) : (
+        <Skeleton />
+      )}
+    </>
+  );
+}

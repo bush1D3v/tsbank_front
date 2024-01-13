15ca2678ed27316b19cabaf763faca59973a3137
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-12 21:04:43 -0300

    update(Footer.tsx): Adicionando animacao ao componente

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ function scrollToTop() {
 
 export default function Footer(): ReactElement {
   return (
-    <footer className="text-sm bg-saturatedBlue flex h-[9dvh] justify-between py-2 px-4 relative">
+    <footer className="text-sm bg-saturatedBlue flex h-[9dvh] justify-between py-2 px-4 relative animate-fade-down animate-duration-500 animate-ease-in-out">
       <span className="md:text-xl my-auto">TSBank <br /> Corporation &copy;</span>
       <p className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] text-center md:text-xl">
         Made with 💖 <br /> by <a

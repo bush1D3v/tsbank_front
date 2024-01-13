6cd135d2f988c37abe3bc9025bcf4050d81afe27
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-13 11:01:03 -0300

    refactor(AboutProject.tsx): Melhorando semantica da pagina

--- src/components/AboutProject/AboutProject.tsx
+++ src/components/AboutProject/AboutProject.tsx
@@ -4,11 +4,10 @@ import { Anchor, ArticleList } from "./components";
 import { type ReactElement } from "react";
 
 export default function AboutProject(): ReactElement {
-
   return (
     <main className="min-h-[82dvh] flex flex-col gap-4 text-center animate-fade-down animate-duration-1000 animate-ease-in-out">
       <div className="my-6 flex flex-col gap-4 items-center max-w-7xl m-auto">
-        <article className="lg:flex lg:items-center lg:w-full lg:justify-around">
+        <div className="lg:flex lg:items-center lg:w-full lg:justify-around">
           <h1 className="font-bold text-4xl md:text-6xl lg:text-8xl">Welcome to <br /> TSBank! 🏦💵</h1>
           <ImageSkeletonLoader
             src={lpWelcome}
@@ -16,7 +15,7 @@ export default function AboutProject(): ReactElement {
             loading={"eager"}
             sessionStorageItem={"aboutImageLoaded"}
           />
-        </article>
+        </div>
         <section className="-mt-4 flex flex-col items-center text-lg">
           <h2 className="mb-8 md:mb-12 font-bold text-xl md:text-4xl lg:text-5xl">What is TSBank? 🤔</h2>
           <p className="px-2 md:px-4 lg:px-0 text-justify md:text-center md:text-3xl lg:text-4xl">
@@ -30,17 +29,13 @@ export default function AboutProject(): ReactElement {
         <section className="flex flex-col items-center gap-4 text-lg">
           <h2 className="mb-4 lg:mt-4 font-bold text-xl md:text-4xl lg:text-5xl">Who developed it? 👨‍💻</h2>
           <article className="flex flex-col items-center gap-4 lg:flex-row">
-            <a
-              className="w-2/4 md:w-2/6 lg:w-8/12 rounded-full hover:scale-105 cursor-pointer transition-all delay-75"
+            <a className="w-2/4 md:w-2/6 lg:w-8/12 rounded-full hover:scale-105 cursor-pointer transition-all delay-75"
               href="https://github.com/bush1D3v"
-              target="_blank"
-            >
-              <img
-                className="rounded-full -mt-4"
+              target="_blank">
+              <img className="rounded-full -mt-4"
                 src="https://avatars.githubusercontent.com/u/133554156?v=4"
                 alt="Victor photo"
-                loading="lazy"
-              />
+                loading="lazy" />
             </a>
             <p className="px-2 md:px-4 lg:px-0 text-justify md:text-center md:text-3xl lg:text-4xl">
               My name is Victor Navarro, and I have been a software developer since 2023. I am a software engineering student at college <Anchor

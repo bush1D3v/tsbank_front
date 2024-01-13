1dec7d88e1fe9aa54aae9d261a3d56ca6f28947a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-13 11:08:04 -0300

    feat(HooksList.tsx): Cria componente de lista de hooks para InitialPage

---
+++ src/components/Home/components/InitialPage/components/HooksList.tsx
@@ -0,0 +1,49 @@
+import { type ReactElement } from "react";
+import AnchorLink from "../../../../AnchorLink";
+import { handleLinkClick } from "../../../../../functions";
+
+export default function HooksList(): ReactElement {
+  const {
+    VITE_REACT_APP_WITHDRAW,
+    VITE_REACT_APP_DEPOSIT,
+    VITE_REACT_APP_PIX,
+    VITE_REACT_APP_CARD
+  } = import.meta.env;
+
+  return (
+    <ul className="flex flex-col lg:flex-row gap-5 md:gap-6 lg:gap-8 w-11/12 animate-fade-down animate-duration-700 animate-ease-in-out">
+      <li className="flex w-full">
+        <AnchorLink
+          buttonBg="bg-saturatedBlue hover:bg-transparent"
+          func={handleLinkClick(VITE_REACT_APP_WITHDRAW)}
+          param={VITE_REACT_APP_WITHDRAW}
+          text="WITHDRAW"
+        />
+      </li>
+      <li className="flex w-full">
+        <AnchorLink
+          buttonBg="bg-saturatedBlue hover:bg-transparent"
+          func={handleLinkClick(VITE_REACT_APP_DEPOSIT)}
+          param={VITE_REACT_APP_DEPOSIT}
+          text="DEPOSIT"
+        />
+      </li>
+      <li className="flex w-full">
+        <AnchorLink
+          buttonBg="bg-saturatedBlue hover:bg-transparent"
+          func={handleLinkClick(VITE_REACT_APP_CARD)}
+          param={VITE_REACT_APP_CARD}
+          text="CARD"
+        />
+      </li>
+      <li className="flex w-full">
+        <AnchorLink
+          buttonBg="bg-saturatedBlue hover:bg-transparent"
+          func={handleLinkClick(VITE_REACT_APP_PIX)}
+          param={VITE_REACT_APP_PIX}
+          text="PIX"
+        />
+      </li>
+    </ul>
+  );
+}

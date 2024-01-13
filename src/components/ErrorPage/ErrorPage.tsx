a4aee14712f1e72f89bc23cfc0a57b9817ad52d5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-13 11:09:34 -0300

    update(ErrorPage.tsx): Adicionando animacao a pagina

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -11,7 +11,7 @@ export default function ErrorPage(): ReactElement {
   } = import.meta.env;
 
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center gap-8 text-center">
+    <main className="w-screen h-screen flex flex-col items-center justify-center gap-8 text-center animate-fade animate-duration-500 animate-ease-in-out">
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">Oops! 😯</h1>
       <p className="font-bold text-xl md:text-2xl lg:text-3xl px-6 md:px-10 lg:px-14">
         It seems like you got lost in the financial maze, but don't worry! Our experts are working to resolve this. 👨‍💻<br />
@@ -19,11 +19,9 @@ export default function ErrorPage(): ReactElement {
       </p>
       <p className="text-6xl md:text-7xl lg:text-8xl font-bold">{status}</p>
       <Link to={VITE_REACT_APP_HOME}>
-        <button
-          type="button"
-          className="rounded-3xl bg-whiteBlue text-lg md:text-xl py-4 md:py-5
-          lg:py-6 px-8 md:px-10 lg:px-12 font-bold hover:text-purple transition-all"
-        >
+        <button className="rounded-3xl bg-whiteBlue text-lg md:text-xl py-4 md:py-5
+        lg:py-6 px-8 md:px-10 lg:px-12 font-bold hover:text-purple transition-all"
+          type="button">
           Return to home page
         </button>
       </Link>

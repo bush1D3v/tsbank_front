b166b0bd5ccb877b8e689857fc7dedb4dd0fd8f4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-13 11:04:45 -0300

    update(InitialPage.tsx): Alteracao de nome, pasta raiz, componentizacao e adicao de animacoes

--- src/components/Home/components/InitialPage/InitialPage.tsx
+++ src/components/Home/components/InitialPage/InitialPage.tsx
@@ -1,5 +1,4 @@
-import AnchorLink from "../../AnchorLink";
-import Loading from "../../Loading";
+import Loading from "../../../Loading";
 import { GiMoneyStack } from "react-icons/gi";
 import {
   useState,
@@ -7,29 +6,22 @@ import {
   useEffect
 } from "react";
 import {
-  handleLinkClick,
   jsonUserParser,
   balanceFormat,
   errorReplace,
   createHash,
-} from "../../../functions";
+} from "../../../../functions";
 import {
   getHistory,
   type HistoryData,
   type HistoryResponseProps
-} from "./functions";
+} from "../functions";
 import { useNavigate } from "react-router-dom";
+import { HooksList } from "./components";
 
-export default function Home(): ReactElement {
+export default function InitialPage(): ReactElement {
   const [ historyData, setHistoryData ] = useState<HistoryData[]>([]);
   const [ isLoading, setIsLoading ] = useState(true);
-
-  const {
-    VITE_REACT_APP_WITHDRAW,
-    VITE_REACT_APP_DEPOSIT,
-    VITE_REACT_APP_PIX,
-    VITE_REACT_APP_CARD
-  } = import.meta.env;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -80,57 +72,28 @@ export default function Home(): ReactElement {
 
   return (
     <section className="flex flex-col items-center justify-center gap-8 md:gap-14 lg:w-7/12">
-      <h1 className="text-xl md:text-2xl lg:text-3l xl:text-4xl font-bold text-center">Hello <br /> {user.name}👋</h1>
-      <div className="flex gap-4 justify-center w-full">
-        <GiMoneyStack className="w-10 h-10 md:w-12 md:h-12 lg:w-15 lg:h-15" />
-        <p className="text-2xl md:text-3xl lg:text-4xl">{balance}</p>
-      </div>
-      <ul className="flex flex-col lg:flex-row gap-5 md:gap-6 lg:gap-8 w-11/12">
-        <li className="flex w-full">
-          <AnchorLink
-            buttonBg="bg-saturatedBlue hover:bg-transparent"
-            func={handleLinkClick(VITE_REACT_APP_WITHDRAW)}
-            param={VITE_REACT_APP_WITHDRAW}
-            text="WITHDRAW"
-          />
-        </li>
-        <li className="flex w-full">
-          <AnchorLink
-            buttonBg="bg-saturatedBlue hover:bg-transparent"
-            func={handleLinkClick(VITE_REACT_APP_DEPOSIT)}
-            param={VITE_REACT_APP_DEPOSIT}
-            text="DEPOSIT"
-          />
-        </li>
-        <li className="flex w-full">
-          <AnchorLink
-            buttonBg="bg-saturatedBlue hover:bg-transparent"
-            func={handleLinkClick(VITE_REACT_APP_CARD)}
-            param={VITE_REACT_APP_CARD}
-            text="CARD"
-          />
-        </li>
-        <li className="flex w-full">
-          <AnchorLink
-            buttonBg="bg-saturatedBlue hover:bg-transparent"
-            func={handleLinkClick(VITE_REACT_APP_PIX)}
-            param={VITE_REACT_APP_PIX}
-            text="PIX"
-          />
-        </li>
-      </ul>
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className="md:-mt-8 -mb-6 md:-mb-12 text-lg md:text-xl lg:text-2xl font-bold">&#x25BC; HISTORY &#x25BC;</p>
+          <h1 className="text-xl md:text-2xl lg:text-3l xl:text-4xl font-bold text-center
+          animate-fade-down animate-duration-700 animate-ease-in-out">
+            Hello <br /> {user.name}👋
+          </h1>
+          <div className="flex gap-4 justify-center w-full animate-fade-down animate-duration-700 animate-ease-in-out">
+            <GiMoneyStack className="w-10 h-10 md:w-12 md:h-12 lg:w-15 lg:h-15" />
+            <p className="text-2xl md:text-3xl lg:text-4xl">{balance}</p>
+          </div>
+          <HooksList />
+          <span className="md:-mt-8 -mb-6 md:-mb-12 text-lg md:text-xl lg:text-2xl font-bold
+          animate-fade animate-duration-700 animate-ease-in-out">
+            &#x25BC; HISTORY &#x25BC;
+          </span>
           {typeof historyData === "string" ? (
             <p className="px-4 text-center text-lg md:text-xl lg:text-2xl">{errorReplace(historyData)}</p>
           ) : (
-            <ul
-              className="p-1 flex flex-col gap-4 md:gap-6 lg:gap-4 w-full max-h-[18dvh]
-              md:max-h-[19dvh] overflow-y-scroll lg:w-3/5 xl:w-2/4 animate-fade-down animate-duration-500 animate-ease-in-out"
-            >
+            <ul className="p-1 flex flex-col gap-4 md:gap-6 lg:gap-4 w-full max-h-[18dvh] md:max-h-[19dvh]
+            overflow-y-scroll lg:w-3/5 xl:w-2/4 animate-fade-down animate-duration-500 animate-ease-in-out">
               {[ ...historyData ].reverse().map((history, index) => (
                 <li
                   className="group relative text-center flex gap-2 md:gap-4 justify-center
@@ -142,10 +105,8 @@ export default function Home(): ReactElement {
                 >
                   <p className="font-bold capitalize">{history.description}</p>
                   <p className="text-base md:text-lg lg:text-xl">{history.date}</p>
-                  <span
-                    className="absolute pr-2 right-0 text-whiteBlue text-lg md:text-xl
-                    group-hover:text-desaturatedBlue transition-all delay-75 ease-in-out"
-                  >
+                  <span className="absolute pr-2 right-0 text-whiteBlue text-lg md:text-xl
+                  group-hover:text-desaturatedBlue transition-all delay-75 ease-in-out">
                     &gt;
                   </span>
                 </li>

2d66fffcec1345ec7f07d7a3fe923744e4da84b9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-14 15:24:48 -0300

    feat(DedicationText.tsx): Cria componente de texto dedicatorio

---
+++ src/components/Footer/components/DedicationText.tsx
@@ -0,0 +1,15 @@
+import { type ReactElement } from "react";
+
+export default function DedicationText(): ReactElement {
+  return (
+    <p className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] text-center lg:text-xl">
+      Made with 💖 <br /> by&nbsp;
+      <a className="font-bold underline cursor-pointer hover:text-darkBlue transition-all"
+        href="https://bushi-links.vercel.app/"
+        rel="noreferrer"
+        target="_blank">
+        bush1D3v
+      </a>
+    </p>
+  );
+}

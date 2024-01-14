28b12fb470619fbdcfbebbbcd2189f7c2a0b08bc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-14 15:23:57 -0300

    feat(index.ts): Cria index exportador de componentes do componente Footer

---
+++ src/components/Footer/components/index.ts
@@ -0,0 +1,9 @@
+import ArrowUp from "./ArrowUp";
+import CopyrightText from "./CopyrightText";
+import DedicationText from "./DedicationText";
+
+export {
+  ArrowUp,
+  CopyrightText,
+  DedicationText
+};

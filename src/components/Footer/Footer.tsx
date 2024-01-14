17a6d7de64cafe72aacc857be0b294c3a66646be
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-14 15:22:47 -0300

    update(Footer.tsx): Melhora componentizacao do Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,17 @@
 import { type ReactElement } from "react";
-import { FaArrowUp } from "react-icons/fa";
-
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-}
+import {
+  ArrowUp,
+  CopyrightText,
+  DedicationText
+} from "./components";
 
 export default function Footer(): ReactElement {
   return (
     <footer className="text-sm bg-saturatedBlue flex h-[9dvh] justify-between py-2 px-4
     relative animate-fade-down animate-duration-500 animate-ease-in-out">
-      <span className="md:text-xl my-auto">TSBank <br /> Corporation &copy;</span>
-      <p className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] text-center md:text-xl">
-        Made with 💖 <br /> by <a
-          className="font-bold underline cursor-pointer hover:text-darkBlue transition-all"
-          href="https://linktr.ee/bush1D3v"
-          target="_blank"
-        >
-          bush1D3v
-        </a>
-      </p>
-      <FaArrowUp className="my-auto text-4xl cursor-pointer hover:text-darkBlue transition-all delay-75" onClick={scrollToTop} />
+      <CopyrightText />
+      <DedicationText />
+      <ArrowUp />
     </footer>
   );
 }

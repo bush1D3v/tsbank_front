c08a4259771f97364b5f664111aa2e8d25c44161
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-14 15:24:30 -0300

    feat(CopyrightText.tsx): Cria componente de texto de copyright

---
+++ src/components/Footer/components/CopyrightText.tsx
@@ -0,0 +1,7 @@
+import { type ReactElement } from "react";
+
+export default function CopyrightText(): ReactElement {
+  return (
+    <span className="lg:text-xl self-center">TSBank <br /> Corporation &copy;</span>
+  );
+}

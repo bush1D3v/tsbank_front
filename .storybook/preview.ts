3c01666dfac0708a5ffd83494659fdfe57108cb0
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:21:32 -0300

    update(preview.ts): Adicionando tema dark e layout padrao como 'fullscreen

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,20 @@
-import { type Preview } from '@storybook/react'
-import "../src/styles/globals.scss"
+import { themes } from "@storybook/theming";
+import { type Preview } from "@storybook/react";
+import "../src/styles/globals.scss";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    docs: {
+      theme: themes.dark,
+    },
+    layout: "fullscreen"
   },
 };
 

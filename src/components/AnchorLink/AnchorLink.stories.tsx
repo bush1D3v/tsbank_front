521c42621a0d0558864018b0d20bb0c725008fba
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:30:51 -0300

    feat(AnchorLink.stories.tsx): Cria documentacao do componente AnchorLink

---
+++ src/components/AnchorLink/AnchorLink.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from "@storybook/react";
+import AnchorLink from "./AnchorLink";
+
+const meta: Meta<typeof AnchorLink> = {
+  title: "Components/AnchorLink",
+  component: AnchorLink,
+  decorators: [
+    (Story) => (
+      <div className="flex items-center justify-center mx-[30dvw] md:mx-[40dvw] h-[100dvh]">
+        <Story />
+      </div>
+    )
+  ]
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AnchorLink>;
+
+export const Default: Story = {
+  args: {
+    func: () => { window.alert("The anchor link button has been clicked!"); },
+    text: "Button",
+    param: "test",
+    buttonBg: "bg-saturatedBlue hover:bg-transparent"
+  }
+};

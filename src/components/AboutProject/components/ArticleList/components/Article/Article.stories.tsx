e8c5392daa1e49d0bd947e7825050c64993e94e7
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:28:56 -0300

    feat(Article.stories.tsx): Cria documentacao do componente Article

---
+++ src/components/AboutProject/components/ArticleList/components/Article/Article.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta } from "@storybook/react";
+import Article from "./Article";
+import { lpInterface } from "../../../../../../assets/images";
+
+const meta: Meta<typeof Article> = {
+  title: "Pages/AboutProject/ArticleList/Article",
+  component: Article,
+  decorators: [
+    (Story) => (
+      <div className="flex h-[100dvh] items-center justify-center">
+        <Story />
+      </div>
+    )
+  ],
+  argTypes: {
+    rowDirection: {
+      control: {
+        type: "select"
+      }
+    }
+  }
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    text: `TSBank offers a seamless platform, allowing you to effortlessly navigate through your accounts,
+    make <em> secure transactions</em>, and gain valuable insights into your financial well-being.`,
+    image: lpInterface,
+    imageAlt: "TSBank Interface",
+    rowDirection: "lg:flex-row-reverse"
+  }
+};

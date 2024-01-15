9732a2b8dce261236775159534c0d9f0c107bac3
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:36:42 -0300

    feat(Register.stories.tsx): Cria documentacao do componente Register

---
+++ src/components/FormAuth/components/Register.stories.tsx
@@ -0,0 +1,20 @@
+import { withRouter } from "storybook-addon-react-router-v6";
+import Register from "./Register";
+import { Meta } from "@storybook/react";
+
+const meta: Meta<typeof Register> = {
+  title: "Components/FormAuth/Register",
+  component: Register,
+  decorators: [
+    withRouter,
+    (Story) => (
+      <div className="flex h-[100dvh] items-center justify-center">
+        <Story />
+      </div>
+    )
+  ]
+};
+
+export default meta;
+
+export const Default = {};

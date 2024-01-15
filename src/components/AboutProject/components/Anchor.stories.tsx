6796f0d7634f02f3607f4a73c53dfe39635f45f2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:25:01 -0300

    feat(Anchor.stories.tsx): Cria documentacao do componente Anchor

---
+++ src/components/AboutProject/components/Anchor.stories.tsx
@@ -0,0 +1,27 @@
+import { withRouter } from "storybook-addon-react-router-v6";
+import Anchor from "./Anchor";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Anchor> = {
+  title: "Pages/AboutProject/Anchor",
+  component: Anchor,
+  decorators: [
+    withRouter,
+    (Story) => (
+      <div className="flex h-[100dvh] items-center justify-center">
+        <Story />
+      </div>
+    )
+  ]
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Anchor>;
+
+export const Default: Story = {
+  args: {
+    text: "Learn more",
+    link: "https://www.google.com/"
+  }
+};

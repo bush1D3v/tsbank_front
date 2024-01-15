08418010ad014ba4ea8dd1188eeeee5c3707b963
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:36:53 -0300

    feat(Header.stories.tsx): Cria documentacao do componente Header

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,10 +1,20 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import Header from "./Header";
+import { Meta } from "@storybook/react";
 
-export default {
+const meta: Meta<typeof Header> = {
   title: "Components/Header",
   component: Header,
-  decorators: [ withRouter ]
+  decorators: [
+    withRouter,
+    (Story) => (
+      <div className="h-[100dvh] py-[50dvh]">
+        <Story />
+      </div>
+    )
+  ]
 };
+
+export default meta;
 
 export const Default = {};

7a5c1b482233e993d286d2efbccc86ad94a9b50b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:11:24 -0300

    fix(Modal.tsx): Adicionando estilo de centralização de texto

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -17,7 +17,7 @@ export default function Modal({
 }: ModalProps): ReactElement {
   if (isOpen) {
     return (
-      <div className="fixed top-0 bottom-0 left-0 right-0 bg-[#00000090]">
+      <div className="fixed top-0 bottom-0 left-0 right-0 bg-[#00000090] text-center">
         <div
           className="fixed top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]
           bg-blueBase p-8 rounded-3xl w-11/12 md:w-[52.5%] lg:w-2/5 xl:w-[30%]

873ba709351d98ba7aabaa268c8ce2cb3c569295
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:24:46 -0300

    feat(AboutProject.stories.tsx): Cria documentacao do componente AboutProject

---
+++ src/components/AboutProject/AboutProject.stories.tsx
@@ -0,0 +1,19 @@
+import { withRouter } from "storybook-addon-react-router-v6";
+import Footer from "../Footer";
+import Header from "../Header";
+import AboutProject from "./AboutProject";
+
+export default {
+  title: "Pages/AboutProject",
+  component: AboutProject,
+  decorators: [ withRouter ]
+};
+
+export const Default = {};
+
+export const WithHeaderAndFooter = () =>
+  <>
+    <Header></Header>
+    <AboutProject></AboutProject>
+    <Footer></Footer>
+  </>;

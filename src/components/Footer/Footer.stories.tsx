c3c692861e0f4c92e33da77ff72d331a613b1336
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:31:26 -0300

    feat(Footer.stories.tsx): Cria documentacao do componente Footer

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -1,8 +1,18 @@
+import { Meta } from "@storybook/react";
 import Footer from "./Footer";
 
-export default {
+const meta: Meta<typeof Footer> = {
   title: "Components/Footer",
   component: Footer,
+  decorators: [
+    (Story) => (
+      <div className="h-[100dvh] py-[50dvh]">
+        <Story />
+      </div>
+    )
+  ]
 };
+
+export default meta;
 
 export const Default = {};

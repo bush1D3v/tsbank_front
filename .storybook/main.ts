5091ebeaa58210a04006bfefba7ce069d3548d02
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:21:55 -0300

    update(main.ts): Adicionando valor padrao para posicionamento do painel

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import { type StorybookConfig } from '@storybook/react-vite';
+import { type StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   "stories": [
@@ -8,9 +8,14 @@ const config: StorybookConfig = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "storybook-addon-react-router-v6"
+    "storybook-addon-react-router-v6",
+    {
+      name: "@storybook/addon-knobs",
+      options: {
+        panelPosition: "right",
+      },
+    },
   ],
   "framework": {
     "name": "@storybook/react-vite",

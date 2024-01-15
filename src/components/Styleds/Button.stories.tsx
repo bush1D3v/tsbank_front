1ca5a1c618c589ab49ff107762fbe482a7290c10
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:38:37 -0300

    feat(Button.stories.tsx): Cria documentacao do componente Button

---
+++ src/components/Styleds/Button.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta } from "@storybook/react";
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/Styleds/Button",
+  component: Button,
+  decorators: [
+    (Story) => (
+      <div className="flex items-center justify-center mx-[20dvw] py-[50dvh] max-h-[100dvh]">
+        <Story />
+      </div>
+    )
+  ]
+};
+
+export default meta;
+
+export const Default = () => <Button>Click me</Button>;

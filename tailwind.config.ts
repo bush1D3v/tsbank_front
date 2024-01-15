3f6005bb3f01d7c3039602a4a7dabb62ab3b74a2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:12:11 -0300

    build(tailwind.config.ts): Adicionando plugin tailwindcss-animated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [ "./index.html", "./src/**/*.{js,ts,jsx,tsx}" ],
   theme: {
     extend: {
       colors: {
@@ -21,5 +21,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animated")],
+  plugins: [ require("tailwindcss-animated") ],
 };

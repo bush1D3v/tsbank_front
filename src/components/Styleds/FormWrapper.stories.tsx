30198733efb4d449a0bb7424bf278107fdcbcc4d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-15 18:38:51 -0300

    feat(FormWrapper.stories.tsx): Cria documentacao do componente FormWrapper

---
+++ src/components/Styleds/FormWrapper.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta } from "@storybook/react";
+import { FormWrapper, Button, InputField } from "./index";
+
+const meta: Meta<typeof FormWrapper> = {
+  title: "Components/Styleds/FormWrapper",
+  component: FormWrapper,
+  decorators: [
+    (Story) => (
+      <div className="flex items-center justify-center m-auto h-[100dvh] w-[50dvw]">
+        <Story />
+      </div>
+    )
+  ]
+};
+
+export default meta;
+
+export const Default = () =>
+  <FormWrapper>
+    <h2 className="font-bold text-2xl lg:text-3xl xl:text-4xl">Form Example</h2>
+    <InputField
+      type="email"
+      placeholder="Email"
+    />
+    <Button>Submit</Button>
+  </FormWrapper>;

2d2cd450fff24dd1d17c139dd67f8758726bac0e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 07:57:54 -0300

    update(AnchorLink.tsx): Atualizando centralizacao textual

--- src/components/AnchorLink/AnchorLink.tsx
+++ src/components/AnchorLink/AnchorLink.tsx
@@ -10,8 +10,8 @@ interface AnchorLinkProps {
 
 export default function AnchorLink({ func, text, param, buttonBg }: AnchorLinkProps): ReactElement {
   const className =
-    `px-3 md:py-1 lg:px-3 xl:px-4 ${buttonBg} border-white cursor-pointer rounded-xl border-2
-    hover:scale-110 transition-all delay-75 ease-in-out font-bold w-full text-center`;
+    `px-3 md:py-1 lg:px-3 xl:px-4 ${buttonBg} border-white cursor-pointer rounded-xl border-2 font-bold
+    hover:scale-110 transition-all delay-75 ease-in-out w-full text-center flex justify-center items-center`;
 
   return (
     <a

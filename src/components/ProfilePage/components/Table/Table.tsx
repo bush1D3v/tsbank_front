39c2677214729afd0da93e6330da1f09ab6767dc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 07:58:29 -0300

    feat(Table.tsx): Cria componente de tabela de dados

---
+++ src/components/ProfilePage/components/Table/Table.tsx
@@ -0,0 +1,20 @@
+import { type ReactElement } from "react";
+import { TableRow } from "./components";
+import { jsonUserParser } from "../../../../functions";
+import { CgProfile } from "react-icons/cg";
+
+export default function Table(): ReactElement {
+  const stringUserData = sessionStorage.getItem("userInfo");
+  const jsonUserData = jsonUserParser(stringUserData);
+
+  return (
+    <table className="h-content flex w-full max-w-sm flex-col justify-center gap-2 rounded-md border-2 border-lightGray p-2 md:scale-125 lg:scale-150">
+      <thead className="flex justify-center pb-6">
+        <CgProfile className="h-[10dvh] w-[10dvh]" />
+      </thead>
+      <TableRow label="Email:" value={jsonUserData.user.email} />
+      <TableRow label="Phone:" value={jsonUserData.user.phone} />
+      <TableRow label="Cpf:" value={jsonUserData.user.cpf} />
+    </table>
+  );
+}

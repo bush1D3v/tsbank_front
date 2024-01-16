89e6679727384f5e351864c02e201d3adf10f139
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 07:55:33 -0300

    update(ProfilePage.tsx): Melhorando componentizacao da pagina

--- src/components/ProfilePage/components/ProfilePage.tsx
+++ src/components/ProfilePage/components/ProfilePage.tsx
@@ -1,18 +1,9 @@
 import { type ReactElement } from "react";
-import { CgProfile } from "react-icons/cg";
-import { TableRow } from ".";
-import {
-  handleLinkClick,
-  jsonUserParser,
-  logoutClick
-} from "../../../functions";
+import { handleLinkClick, logoutClick } from "../../../functions";
 import AnchorLink from "../../AnchorLink";
+import Table from "./Table";
 
 export default function ProfilePage(): ReactElement {
-  const stringUserData = sessionStorage.getItem("userInfo");
-
-  const jsonUserData = jsonUserParser(stringUserData);
-
   const {
     VITE_REACT_APP_LOGIN,
     VITE_REACT_APP_UPDATE
@@ -20,14 +11,7 @@ export default function ProfilePage(): ReactElement {
 
   return (
     <div className="flex justify-center items-center h-[82dvh] flex-col gap-10 md:gap-14 lg:gap-20 p-2">
-      <table className="h-content flex w-full max-w-sm flex-col justify-center gap-2 rounded-md border-2 border-lightGray p-2 md:scale-125 lg:scale-150">
-        <thead className="flex justify-center pb-6">
-          <CgProfile className="h-[10dvh] w-[10dvh]" />
-        </thead>
-        <TableRow label="Email:" value={jsonUserData.user.email} />
-        <TableRow label="Phone:" value={jsonUserData.user.phone} />
-        <TableRow label="Cpf:" value={jsonUserData.user.cpf} />
-      </table>
+      <Table />
       <ul className="flex gap-8 w-full px-2">
         <li className="flex w-full">
           <AnchorLink

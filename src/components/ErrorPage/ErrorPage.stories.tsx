9e44b5fb721d894231a653d418d4305d462a691f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 14:50:00 -0300

    refactor(ErrorPage.stories.tsx): Refatorando nome da pagina na documentacao

--- src/components/ErrorPage/ErrorPage.stories.tsx
+++ src/components/ErrorPage/ErrorPage.stories.tsx
@@ -2,7 +2,7 @@ import { withRouter } from "storybook-addon-react-router-v6";
 import ErrorPage from "./ErrorPage";
 
 export default {
-  title: "Components/ErrorPage",
+  title: "Pages/ErrorPage",
   component: ErrorPage,
   decorators: [ withRouter ]
 };

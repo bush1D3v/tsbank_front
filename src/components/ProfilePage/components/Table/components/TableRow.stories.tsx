f69a1e903571baa61223e21fd9540d1e02fa1e8f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 08:06:44 -0300

    feat(TableRow.stories.tsx): Cria documentacao do componente TableRow

---
+++ src/components/ProfilePage/components/Table/components/TableRow.stories.tsx
@@ -0,0 +1,27 @@
+import { withRouter } from "storybook-addon-react-router-v6";
+import TableRow from "./TableRow";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof TableRow> = {
+  title: "Pages/ProfilePage/Table/TableRow",
+  component: TableRow,
+  decorators: [
+    withRouter,
+    (Story) => (
+      <div className="h-[100dvh] flex items-center justify-center">
+        <Story />
+      </div>
+    )
+  ]
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TableRow>;
+
+export const Default: Story = {
+  args: {
+    label: "Email",
+    value: "emailDeTeste@gmail.com"
+  }
+};

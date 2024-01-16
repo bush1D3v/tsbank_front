01e29c9ca550bfde59a1621f83b42faec5f3bb5e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 08:04:58 -0300

    feat(ProfilePage.stories.tsx): Cria documentacao da pagina de profile

---
+++ src/components/ProfilePage/components/ProfilePage.stories.tsx
@@ -0,0 +1,28 @@
+import { withRouter } from "storybook-addon-react-router-v6";
+import ProfilePage from "./ProfilePage";
+import { Meta } from "@storybook/react";
+import Header from "../../Header";
+import Footer from "../../Footer";
+
+const meta: Meta<typeof ProfilePage> = {
+  title: "Pages/ProfilePage",
+  component: ProfilePage,
+  decorators: [ withRouter ]
+};
+
+export default meta;
+
+export const Default = () =>
+  <div className="m-auto max-w-[500px]">
+    <ProfilePage>
+    </ProfilePage>
+  </div>;
+
+export const WithHeaderAndFooter = () =>
+  <>
+    <Header />
+    <div className="m-auto max-w-[500px]">
+      <ProfilePage></ProfilePage>
+    </div>
+    <Footer />
+  </>;

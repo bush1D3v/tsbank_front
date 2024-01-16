9e296cfc95980929289a20ccace8e0884924e8a1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 20:59:52 -0300

    refactor(Modal.tsx): Alterando sessionStorage de userInfo para userData

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -21,9 +21,7 @@ export default function Modal({
         <div
           className="fixed top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]
           bg-blueBase p-8 rounded-3xl w-11/12 md:w-[52.5%] lg:w-2/5 xl:w-[30%]
-          h-3/6 md:h-[35%] lg:h-2/5 flex flex-col justify-evenly
-        "
-        >
+          h-3/6 md:h-[35%] lg:h-2/5 flex flex-col justify-evenly">
           <h2 className="text-4xl mb-2 font-bold text-white">{title}</h2>
           <p className="text-white mb-3 text-2xl">{description}</p>
           <button
@@ -31,8 +29,7 @@ export default function Modal({
             className="text-white px-7 py-2 bg-whiteBlue font-bold rounded-[10px] text-xl hover:text-purple transition-colors"
             onClick={() => {
               setOpen(!isOpen);
-            }}
-          >
+            }}>
             {btnMessage}
           </button>
         </div>

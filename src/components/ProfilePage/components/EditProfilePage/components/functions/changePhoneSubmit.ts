5b29cfde630dfc6da24ad2571fa1ddf4c89f3f97
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 21:01:15 -0300

    refactor(changePhoneSubmit.ts): Alterando sessionStorage de userInfo para userData

--- src/components/ProfilePage/components/EditProfilePage/components/functions/changePhoneSubmit.ts
+++ src/components/ProfilePage/components/EditProfilePage/components/functions/changePhoneSubmit.ts
@@ -16,7 +16,7 @@ export default async function changePhoneSubmit(
 ): Promise<changePhoneResponseProps> {
   const { userData } = data;
 
-  const { token } = jsonUserParser(sessionStorage.getItem("userInfo"));
+  const { token } = jsonUserParser(sessionStorage.getItem("userData"));
 
   const {
     VITE_REACT_APP_API_BASE_URL,

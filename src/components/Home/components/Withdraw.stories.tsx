1c62981624d7675d13ef4e0ae9290021ab855411
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 22:04:45 -0300

    update(Withdraw.stories.tsx): Adiciona documentacao de componente com layout ao redor

--- src/components/Home/components/Withdraw.stories.tsx
+++ src/components/Home/components/Withdraw.stories.tsx
@@ -1,6 +1,8 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import Withdraw from "./Withdraw";
 import { Meta } from "@storybook/react";
+import Footer from "../../Footer";
+import Header from "../../Header";
 
 const meta: Meta<typeof Withdraw> = {
   title: "Pages/InitialPage/Withdraw",
@@ -18,3 +20,12 @@ const meta: Meta<typeof Withdraw> = {
 export default meta;
 
 export const Default = {};
+
+export const WithHeaderAndFooter = () =>
+  <div className="h-[100dvh] w-[100dvw] flex justify-between flex-col">
+    <Header />
+    <div className="flex justify-center">
+      <Withdraw />
+    </div>
+    <Footer />
+  </div>;

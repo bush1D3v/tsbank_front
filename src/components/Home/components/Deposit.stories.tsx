41930d4cd39860cbf46cb473b39fc6ed01a17e5c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 22:03:57 -0300

    update(Deposit.stories.tsx): Adiciona documentacao de componente com layout ao redor

--- src/components/Home/components/Deposit.stories.tsx
+++ src/components/Home/components/Deposit.stories.tsx
@@ -1,6 +1,8 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import Deposit from "./Deposit";
 import { Meta } from "@storybook/react";
+import Footer from "../../Footer";
+import Header from "../../Header";
 
 const meta: Meta<typeof Deposit> = {
   title: "Pages/InitialPage/Deposit",
@@ -18,3 +20,12 @@ const meta: Meta<typeof Deposit> = {
 export default meta;
 
 export const Default = {};
+
+export const WithHeaderAndFooter = () =>
+  <div className="h-[100dvh] w-[100dvw] flex justify-between flex-col">
+    <Header />
+    <div className="flex justify-center">
+      <Deposit />
+    </div>
+    <Footer />
+  </div>;

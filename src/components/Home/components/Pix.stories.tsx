c86f0e18aa3eb57b1ff6520c3634845356608f88
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 22:04:04 -0300

    update(Pix.stories.tsx): Adiciona documentacao de componente com layout ao redor

--- src/components/Home/components/Pix.stories.tsx
+++ src/components/Home/components/Pix.stories.tsx
@@ -1,6 +1,8 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import Pix from "./Pix";
 import { Meta } from "@storybook/react";
+import Footer from "../../Footer";
+import Header from "../../Header";
 
 const meta: Meta<typeof Pix> = {
   title: "Pages/InitialPage/Pix",
@@ -18,3 +20,12 @@ const meta: Meta<typeof Pix> = {
 export default meta;
 
 export const Default = {};
+
+export const WithHeaderAndFooter = () =>
+  <div className="h-[100dvh] w-[100dvw] flex justify-between flex-col">
+    <Header />
+    <div className="flex justify-center">
+      <Pix />
+    </div>
+    <Footer />
+  </div>;

5dc67c982cfa25c3643ba7b4ae85c092df98b118
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-17 19:02:05 -0300

    build(vite.config.ts): Adicionando configuracao do vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   base: "/",
   plugins: [ react() ],
   server: {

e7204d82f04ce160edbd50dcdd63fb5597105661
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 22:05:18 -0300

    update(ChangeEmail.stories.tsx): Adiciona documentacao de componente com layout ao redor

--- src/components/ProfilePage/components/EditProfilePage/components/ChangeEmail.stories.tsx
+++ src/components/ProfilePage/components/EditProfilePage/components/ChangeEmail.stories.tsx
@@ -1,6 +1,8 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import ChangeEmail from "./ChangeEmail";
 import { Meta } from "@storybook/react";
+import Footer from "../../../../Footer";
+import Header from "../../../../Header";
 
 const meta: Meta<typeof ChangeEmail> = {
   title: "Pages/ProfilePage/EditProfilePage/ChangeEmail",
@@ -13,4 +15,13 @@ export default meta;
 export const Default = () =>
   <div className="h-[100dvh] flex items-center justify-center">
     <ChangeEmail />
+  </div>;
+
+export const WithHeaderAndFooter = () =>
+  <div className="h-[100dvh] flex justify-between flex-col">
+    <Header />
+    <div className="flex justify-center">
+      <ChangeEmail />
+    </div>
+    <Footer />
   </div>;

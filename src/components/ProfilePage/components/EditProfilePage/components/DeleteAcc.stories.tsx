70a1c940f6fc632e1d12d020739609d8c86e259e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 22:05:52 -0300

    update(DeleteAcc.stories.tsx): Adiciona documentacao de componente com layout ao redor

--- src/components/ProfilePage/components/EditProfilePage/components/DeleteAcc.stories.tsx
+++ src/components/ProfilePage/components/EditProfilePage/components/DeleteAcc.stories.tsx
@@ -1,6 +1,8 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import DeleteAcc from "./DeleteAcc";
 import { Meta } from "@storybook/react";
+import Footer from "../../../../Footer";
+import Header from "../../../../Header";
 
 const meta: Meta<typeof DeleteAcc> = {
   title: "Pages/ProfilePage/EditProfilePage/DeleteAcc",
@@ -13,4 +15,13 @@ export default meta;
 export const Default = () =>
   <div className="h-[100dvh] flex items-center justify-center">
     <DeleteAcc />
+  </div>;
+
+export const WithHeaderAndFooter = () =>
+  <div className="h-[100dvh] flex justify-between flex-col">
+    <Header />
+    <div className="flex justify-center">
+      <DeleteAcc />
+    </div>
+    <Footer />
   </div>;

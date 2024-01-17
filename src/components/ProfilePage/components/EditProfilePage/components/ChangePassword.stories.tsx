49b51e30d3f1e67bb1d6a8d425ec54cb064a4fdc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 22:05:26 -0300

    update(ChangePassword.stories.tsx): Adiciona documentacao de componente com layout ao redor

--- src/components/ProfilePage/components/EditProfilePage/components/ChangePassword.stories.tsx
+++ src/components/ProfilePage/components/EditProfilePage/components/ChangePassword.stories.tsx
@@ -1,6 +1,8 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import ChangePassword from "./ChangePassword";
 import { Meta } from "@storybook/react";
+import Header from "../../../../Header";
+import Footer from "../../../../Footer";
 
 const meta: Meta<typeof ChangePassword> = {
   title: "Pages/ProfilePage/EditProfilePage/ChangePassword",
@@ -13,4 +15,13 @@ export default meta;
 export const Default = () =>
   <div className="h-[100dvh] flex items-center justify-center">
     <ChangePassword />
+  </div>;
+
+export const WithHeaderAndFooter = () =>
+  <div className="h-[100dvh] flex justify-between flex-col">
+    <Header />
+    <div className="flex justify-center">
+      <ChangePassword />
+    </div>
+    <Footer />
   </div>;

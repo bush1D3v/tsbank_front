908bcc7797efcb4c59b0cfc3215463f9a0d66d8c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 21:01:01 -0300

    refactor(deleteAccSubmit.ts): Alterando sessionStorage de userInfo para userData

--- src/components/ProfilePage/components/EditProfilePage/components/functions/deleteAccSubmit.ts
+++ src/components/ProfilePage/components/EditProfilePage/components/functions/deleteAccSubmit.ts
@@ -12,7 +12,7 @@ export default async function deleteAccSubmit(
 ): Promise<deleteAccResponseProps> {
   const { userData } = data;
 
-  const { token } = jsonUserParser(sessionStorage.getItem("userInfo"));
+  const { token } = jsonUserParser(sessionStorage.getItem("userData"));
 
   const {
     VITE_REACT_APP_API_BASE_URL,

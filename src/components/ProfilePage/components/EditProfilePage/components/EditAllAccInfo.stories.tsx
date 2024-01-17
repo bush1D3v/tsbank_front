2fa96c7331eb82970a847d46fdd85ed1bae6b9f4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-16 22:05:43 -0300

    update(EditAllAccInfo.stories.tsx): Adiciona documentacao de componente com layout ao redor

--- src/components/ProfilePage/components/EditProfilePage/components/EditAllAccInfo.stories.tsx
+++ src/components/ProfilePage/components/EditProfilePage/components/EditAllAccInfo.stories.tsx
@@ -1,6 +1,8 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import EditAllAccInfo from "./EditAllAccInfo";
 import { Meta } from "@storybook/react";
+import Header from "../../../../Header";
+import Footer from "../../../../Footer";
 
 const meta: Meta<typeof EditAllAccInfo> = {
   title: "Pages/ProfilePage/EditProfilePage/EditAllAccInfo",
@@ -13,4 +15,13 @@ export default meta;
 export const Default = () =>
   <div className="h-[100dvh] flex items-center justify-center">
     <EditAllAccInfo />
+  </div>;
+
+export const WithHeaderAndFooter = () =>
+  <div className="h-[100dvh] flex justify-between flex-col">
+    <Header />
+    <div className="flex justify-center">
+      <EditAllAccInfo />
+    </div>
+    <Footer />
   </div>;

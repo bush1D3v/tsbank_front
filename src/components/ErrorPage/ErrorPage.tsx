279c94e99283d5fdaf78e72aabf859f8b78fde59
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-18 22:25:36 -0300

    update(ErrorPage.tsx): Adicionando data-testid

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -17,14 +17,15 @@ export default function ErrorPage({ error }: ErrorPageStorybookProps): ReactElem
   } = import.meta.env;
 
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center gap-8 text-center animate-fade animate-duration-500 animate-ease-in-out">
+    <main className="w-screen h-screen flex flex-col items-center justify-center gap-8 text-center animate-fade animate-duration-500 animate-ease-in-out"
+      data-testid="ErrorPage">
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">Oops! 😯</h1>
       <p className="font-bold text-xl md:text-2xl lg:text-3xl px-6 md:px-10 lg:px-14">
         It seems like you got lost in the financial maze, but don't worry! Our experts are working to resolve this. 👨‍💻<br />
         In the meantime, check out our amazing resources! 🚀🌟
       </p>
       <p className="text-6xl md:text-7xl lg:text-8xl font-bold">{status}</p>
-      <Link to={VITE_REACT_APP_HOME}>
+      <Link data-testid="ErrorPageLink" to={VITE_REACT_APP_HOME}>
         <button className="rounded-3xl bg-whiteBlue text-lg md:text-xl py-4 md:py-5
         lg:py-6 px-8 md:px-10 lg:px-12 font-bold hover:text-purple transition-all"
           type="button">

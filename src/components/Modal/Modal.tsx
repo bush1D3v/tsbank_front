bf08ef4195870d086b1fa940d18bcf610df2ab80
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-18 22:25:21 -0300

    update(Modal.tsx): Adicionando data-testid

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -17,7 +17,7 @@ export default function Modal({
 }: ModalProps): ReactElement {
   if (isOpen) {
     return (
-      <div className="fixed top-0 bottom-0 left-0 right-0 bg-[#00000090] text-center">
+      <div className="fixed top-0 bottom-0 left-0 right-0 bg-[#00000090] text-center" data-testid="Modal">
         <div
           className="fixed top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]
           bg-blueBase p-8 rounded-3xl w-11/12 md:w-[52.5%] lg:w-2/5 xl:w-[30%]
@@ -27,6 +27,7 @@ export default function Modal({
           <button
             type="button"
             className="text-white px-7 py-2 bg-whiteBlue font-bold rounded-[10px] text-xl hover:text-purple transition-colors"
+            data-testid="ModalButton"
             onClick={() => {
               setOpen(!isOpen);
             }}>

97c89b56c2c8fcbb2d497ea7a8fbbea7418f95c7
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-19 21:09:09 -0300

    test(NavLinks.test.tsx): Cria teste para o componente NavLinks

---
+++ src/components/Header/components/NavLinks.test.tsx
@@ -0,0 +1,99 @@
+import "@testing-library/jest-dom";
+import {
+  fireEvent,
+  screen,
+  render
+} from "@testing-library/react";
+import {
+  describe,
+  test,
+  expect,
+  vi,
+  beforeEach,
+} from "vitest";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import NavLinks from "./NavLinks";
+import { handleLinkClick } from "../../../functions";
+
+const {
+  VITE_REACT_APP_HOME,
+  VITE_REACT_APP_ABOUT
+} = import.meta.env;
+
+const NavLinksTestId: string = "NavLinks";
+const NavLinksListTestId: string = "NavLinksList";
+const NavLinksListItemHomeTestId: string = "NavLinksListItemHome";
+const NavLinksListItemAboutTestId: string = "NavLinksListItemAbout";
+
+const testHomeText: string = "HOME";
+const testAboutText: string = "ABOUT";
+
+vi.mock("../../../functions", () => ({
+  handleLinkClick: vi.fn(),
+  eventClick: vi.fn()
+}));
+
+const router = createMemoryRouter(
+  [ { path: "/", element: <NavLinks /> } ],
+  { initialEntries: [ "/" ], }
+);
+
+describe("NavLinks component tests", () => {
+  beforeEach(() => {
+    render(<RouterProvider router={router} />);
+  });
+
+  test("Should be able to render the component correctly", () => {
+    expect(screen.getByTestId(NavLinksTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the component with the correctly List", () => {
+    expect(screen.getByTestId(NavLinksListTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the List component with the correctly tailwind classes", () => {
+    expect(screen.getByTestId(NavLinksListTestId)).toHaveClass(`
+      flex gap-6 font-bold
+    `);
+  });
+
+  test("Should be able to render the component with the correctly ListItemHome", () => {
+    expect(screen.getByTestId(NavLinksListItemHomeTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the ListItemHome with the correctly tailwind classes", () => {
+    expect(screen.getByTestId(NavLinksListItemHomeTestId)).toHaveClass(`
+      flex
+    `);
+  });
+
+  test("ListItemHome should be able to fire event", () => {
+    fireEvent.click(screen.getByTestId(NavLinksListItemHomeTestId));
+
+    expect(handleLinkClick).toHaveBeenCalledWith(VITE_REACT_APP_HOME);
+  });
+
+  test("Should be able to render the ListItemHome with the correctly text", () => {
+    expect(screen.getByTestId(NavLinksListItemHomeTestId)).toHaveTextContent(testHomeText);
+  });
+
+  test("Should be able to render the component with the correctly ListItemAbout", () => {
+    expect(screen.getByTestId(NavLinksListItemAboutTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the ListItemAbout with the correctly tailwind classes", () => {
+    expect(screen.getByTestId(NavLinksListItemAboutTestId)).toHaveClass(`
+      flex
+    `);
+  });
+
+  test("ListItemHome should be able to fire event", () => {
+    fireEvent.click(screen.getByTestId(NavLinksListItemHomeTestId));
+
+    expect(handleLinkClick).toHaveBeenCalledWith(VITE_REACT_APP_ABOUT);
+  });
+
+  test("Should be able to render the ListItemAbout with the correctly text", () => {
+    expect(screen.getByTestId(NavLinksListItemAboutTestId)).toHaveTextContent(testAboutText);
+  });
+});

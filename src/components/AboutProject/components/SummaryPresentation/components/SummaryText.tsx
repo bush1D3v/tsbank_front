8dc365945ed076c71f40a3d60341b902357c2b7a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 14:29:23 -0300

    feat(SummaryText.tsx): Cria componente de texto para SummaryPresentation

---
+++ src/components/AboutProject/components/SummaryPresentation/components/SummaryText.tsx
@@ -0,0 +1,18 @@
+import { type ReactElement } from "react";
+import Anchor from "./Anchor";
+
+export default function SummaryText(): ReactElement {
+  return (
+    <p className="px-2 md:px-4 lg:px-0 text-justify md:text-center md:text-3xl lg:text-4xl">
+      My name is Victor Navarro, and I have been a software developer since 2023. I am a software engineering student at college <Anchor
+        text="Cruzeiro do Sul"
+        link="https://www.cruzeirodosulvirtual.com.br/"
+      />
+      , and I work as a back-end developer at <Anchor
+        text=" Nasajon"
+        link="https://nasajon.com.br/"
+      />
+      .
+    </p>
+  );
+}

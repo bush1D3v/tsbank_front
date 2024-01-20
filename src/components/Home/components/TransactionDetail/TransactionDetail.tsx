86278504b27b5cd181b85b49cb8fdb11567ad030
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 11:39:06 -0300

    update(TransactionDetail.tsx): Atualiza pasta raiz do arquivo

--- src/components/Home/components/TransactionDetail/TransactionDetail.tsx
+++ src/components/Home/components/TransactionDetail/TransactionDetail.tsx
@@ -1,16 +1,17 @@
-import Loading from "../../Loading";
+import Loading from "../../../Loading";
 import { useNavigate, useParams } from "react-router-dom";
-import { balanceFormat, decryptHash } from "../../../functions";
+import { decryptHash } from "../../../../functions";
 import {
   transactionDetailSubmit,
   type SummaryResponseProps,
   type SummaryData
-} from "./functions";
+} from "../functions";
 import {
   useEffect,
   type ReactElement,
   useState
 } from "react";
+import { TransactionModal } from "./components";
 
 /* Interface criada para exibir dados na documentação Storybook,
 sem forçar um valor aleatório diretamente no componente */
@@ -62,30 +63,9 @@ export default function TransactionDetail({ fakeData }: TransactionDetailStorybo
           {typeof transactionData === "string" ? (
             <p className="text-xl md:text-2xl lg:text-3xl font-bold">{transactionData}</p>
           ) : (
-            <article className="flex flex-col gap-4 justify-center items-center text-xl
-            font-bold animate-fade-down animate-ease-in-out animate-500">
-              <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-14">TRANSACTION DETAILS</h1>
-              <div className="text-center flex flex-col gap-2 md:gap-4
-                bg-saturatedBlue rounded-2xl p-4 md:text-xl lg:text-2xl
-                cursor-pointer hover:bg-darkBlue border-2 border-darkBlue
-                transition-all scale-125 hover:scale-150 delay-75 ease-in-out
-                capitalize">
-                <p>{transactionData?.description}</p>
-                <p>
-                  {
-                    transactionData?.type === "output" ? " - " :
-                      transactionData?.type === "input" ? " + " : ""
-                  }
-                  {
-                    balanceFormat(typeof transactionData?.value === "number" ?
-                      transactionData?.value : 0)
-                  }
-                </p>
-                <p>
-                  {transactionData?.date}
-                </p>
-              </div>
-            </article>
+            <TransactionModal
+              transactionData={transactionData}
+            />
           )}
         </>
       )}

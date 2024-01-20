666e78e498284b9b089a3e5242dc30244f0a47e2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-19 21:05:01 -0300

    update(DedicationText.tsx): Adicionando valores de data-testid

--- src/components/Footer/components/DedicationText.tsx
+++ src/components/Footer/components/DedicationText.tsx
@@ -2,12 +2,13 @@ import { type ReactElement } from "react";
 
 export default function DedicationText(): ReactElement {
   return (
-    <p className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] text-center lg:text-xl">
+    <p className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] text-center lg:text-xl" data-testid="DedicationText">
       Made with 💖 <br /> by&nbsp;
       <a className="font-bold underline cursor-pointer hover:text-darkBlue transition-all"
         href="https://bushi-links.vercel.app/"
         rel="noreferrer"
-        target="_blank">
+        target="_blank"
+        data-testid="DedicationTextLink">
         bush1D3v
       </a>
     </p>

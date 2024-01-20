aeb20af68fceb97d7b82154bb689511b307aeef4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 14:20:28 -0300

    refactor(CardTransaction.stories.tsx): Atualiza pasta raiz do arquivo

--- src/components/Home/components/Card/components/CardTransaction.stories.tsx
+++ src/components/Home/components/Card/components/CardTransaction.stories.tsx
@@ -1,6 +1,6 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import CardTransaction from "./CardTransaction";
-import { Meta } from "@storybook/react";
+import { type Meta } from "@storybook/react";
 import Footer from "../../../../Footer";
 import Header from "../../../../Header";
 

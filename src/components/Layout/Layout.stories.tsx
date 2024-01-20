5037091b1803d9acd70d83773d59958ab3d46cfb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-19 21:03:03 -0300

    feat(Layout.stories.tsx): Cria documentacao do componente Layout

---
+++ src/components/Layout/Layout.stories.tsx
@@ -0,0 +1,17 @@
+import { withRouter } from "storybook-addon-react-router-v6";
+import Layout from "./Layout";
+import { Meta } from "@storybook/react";
+import MainProfile from "../ProfilePage";
+
+const meta: Meta<typeof Layout> = {
+  title: "Layout",
+  component: Layout,
+  decorators: [ withRouter ]
+};
+
+export default meta;
+
+export const Default = () =>
+  <Layout>
+    <MainProfile />
+  </Layout>;

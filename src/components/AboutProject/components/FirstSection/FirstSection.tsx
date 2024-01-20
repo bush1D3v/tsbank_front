0b10cfdff0efa0dab7c3f3212c5c886249ec70e7
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 19:39:28 -0300

    update(FirstSection.tsx): Adicionando data-testid ao componente

--- src/components/AboutProject/components/FirstSection/FirstSection.tsx
+++ src/components/AboutProject/components/FirstSection/FirstSection.tsx
@@ -3,14 +3,16 @@ import { ArticleList } from "./components";
 
 export default function FirstSection(): ReactElement {
   return (
-    <section className="-mt-4 flex flex-col items-center text-lg">
-      <h2 className="mb-8 md:mb-12 font-bold text-xl md:text-4xl lg:text-5xl">
+    <section className="-mt-4 flex flex-col items-center text-lg" data-testid="FirstSection">
+      <h2 className="mb-8 md:mb-12 font-bold text-xl md:text-4xl lg:text-5xl"
+        data-testid="FirstSectionSubtitle">
         What is TSBank? 🤔
       </h2>
-      <p className="px-2 md:px-4 lg:px-0 text-justify md:text-center md:text-3xl lg:text-4xl">
+      <p className="px-2 md:px-4 lg:px-0 text-justify md:text-center md:text-3xl lg:text-4xl"
+        data-testid="FirstSectionParagraph">
         TSBank is <u>not just a bank</u>; it's a financial companion meticulously crafted to
-        <strong>redefine</strong> the way you experience banking. Imagine a world where managing your
-        finances is not just a task, but an effortless and enjoyable part of your daily routine."
+        <strong> redefine</strong> the way you experience banking. Imagine a world where managing your
+        finances is not just a task, but an effortless and enjoyable part of your daily routine.
       </p>
       <ArticleList />
     </section>

f64ec230d8b7daa95bdb7706cfd416daf63b4546
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 11:40:05 -0300

    feat(TransactionModal.stories.tsx): Cria arquivo de documentacao do componente TransactionModal

---
+++ src/components/Home/components/TransactionDetail/components/TransactionModal.stories.tsx
@@ -0,0 +1,28 @@
+import TransactionModal from "./TransactionModal";
+import { Meta } from "@storybook/react";
+
+const meta: Meta<typeof TransactionModal> = {
+  title: "Pages/InitialPage/TransactionDetail/TransactionModal",
+  component: TransactionModal,
+  decorators: [
+    (Story) => (
+      <div className="h-[100dvh] flex items-center justify-center">
+        <Story />
+      </div>
+    )
+  ]
+};
+
+export default meta;
+
+export const Default = () =>
+  <TransactionModal
+    transactionData={{
+      id: 1,
+      description: "deposit",
+      value: 1000,
+      date: "2024/01/08 21:57:59",
+      user_id: 71,
+      type: "input"
+    }}
+  />;

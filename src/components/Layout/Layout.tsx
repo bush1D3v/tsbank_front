80c9f01d2af648cc44ef57a5fcf0bd3d30115245
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-19 21:04:38 -0300

    update(Layout.tsx): Adicionando valores de data-testid

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,12 +4,18 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
 
-export default function Layout(): ReactElement {
+/* Interface criada para exibir componente na documentação Storybook,
+sem forçar um valor aleatório diretamente no componente */
+interface LayoutStorybookProps {
+  children?: ReactElement
+}
+
+export default function Layout({ children }: LayoutStorybookProps): ReactElement {
   return (
-    <>
+    <div data-testid="Layout">
       <Header />
-      <Outlet />
+      {typeof children !== "undefined" ? (children) : (<Outlet />)}
       <Footer />
-    </>
+    </div>
   );
 }

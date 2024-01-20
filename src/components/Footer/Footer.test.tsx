deb0b606d4ac5932c4ed84d58f1d41cd37169f81
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-19 21:07:43 -0300

    test(Footer.test.tsx): Cria teste para o componente Footer

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,43 @@
+import "@testing-library/jest-dom";
+import { screen, render } from "@testing-library/react";
+import {
+  describe,
+  test,
+  expect,
+  beforeEach,
+} from "vitest";
+import Footer from "./Footer";
+
+const FooterTestId: string = "Footer";
+const ArrowUpTestId: string = "ArrowUp";
+const CopyrightTextTestId: string = "CopyrightText";
+const DedicationTextTestId: string = "DedicationText";
+
+describe("Footer component tests", () => {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+
+  test("Should be able to render the component correctly", () => {
+    expect(screen.getByTestId(FooterTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the component with the correctly tailwind classes", () => {
+    expect(screen.getByTestId(FooterTestId)).toHaveClass(`
+      text-sm bg-saturatedBlue flex h-[9dvh] justify-between py-2 px-4
+      relative animate-fade-down animate-duration-500 animate-ease-in-out
+    `);
+  });
+
+  test("Should be able to render the componente with the ArrowUp component", () => {
+    expect(screen.getByTestId(ArrowUpTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the componente with the CopyrightText component", () => {
+    expect(screen.getByTestId(CopyrightTextTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the componente with the DedicationText component", () => {
+    expect(screen.getByTestId(DedicationTextTestId)).toBeInTheDocument();
+  });
+});

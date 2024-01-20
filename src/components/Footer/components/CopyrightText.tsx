e8e8cd149237e70be3f04893e3f81fe9fd969dcc
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-19 21:05:14 -0300

    update(CopyrightText.tsx): Adicionando valores de data-testid

--- src/components/Footer/components/CopyrightText.tsx
+++ src/components/Footer/components/CopyrightText.tsx
@@ -2,6 +2,11 @@ import { type ReactElement } from "react";
 
 export default function CopyrightText(): ReactElement {
   return (
-    <span className="lg:text-xl self-center">TSBank <br /> Corporation &copy;</span>
+    <span
+      className="lg:text-xl self-center"
+      data-testid="CopyrightText"
+    >
+      TSBank <br /> Corporation &copy;
+    </span>
   );
 }

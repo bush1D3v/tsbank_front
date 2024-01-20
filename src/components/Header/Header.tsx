57a032dc39273c5b6cf1effa9369fbfec9f05b1f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-19 21:04:30 -0300

    update(Header.tsx): Adicionando valores de data-testid

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,8 @@ import { Logo, NavLinks, Profile } from "./components";
 
 export default function Header(): ReactElement {
   return (
-    <header className="bg-saturatedBlue flex h-[9dvh] items-center justify-between py-2 px-4 animate-fade-down animate-duration-500 animate-ease-in-out">
+    <header className="bg-saturatedBlue flex h-[9dvh] items-center justify-between py-2 px-4 animate-fade-down animate-duration-500 animate-ease-in-out"
+      data-testid="Header">
       <Logo />
       <NavLinks />
       <Profile />

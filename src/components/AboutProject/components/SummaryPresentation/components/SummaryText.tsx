e2edbbf6b81fd6047c69ef0662563af739e6620c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 19:38:21 -0300

    update(SummaryText.tsx): Adicionando data-testid ao componente

--- src/components/AboutProject/components/SummaryPresentation/components/SummaryText.tsx
+++ src/components/AboutProject/components/SummaryPresentation/components/SummaryText.tsx
@@ -3,7 +3,8 @@ import Anchor from "./Anchor";
 
 export default function SummaryText(): ReactElement {
   return (
-    <p className="px-2 md:px-4 lg:px-0 text-justify md:text-center md:text-3xl lg:text-4xl">
+    <p className="px-2 md:px-4 lg:px-0 text-justify md:text-center md:text-3xl lg:text-4xl"
+      data-testid="SummaryText">
       My name is Victor Navarro, and I have been a software developer since 2023. I am a software engineering student at college <Anchor
         text="Cruzeiro do Sul"
         link="https://www.cruzeirodosulvirtual.com.br/"

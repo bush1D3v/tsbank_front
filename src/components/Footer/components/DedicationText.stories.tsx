ab0a4fb43e440391d9f3d1db1854abb5a28fa332
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 14:21:38 -0300

    refactor(DedicationText.stories.tsx): Atualiza pasta raiz do arquivo

--- src/components/Footer/components/DedicationText.stories.tsx
+++ src/components/Footer/components/DedicationText.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta } from "@storybook/react";
+import { type Meta } from "@storybook/react";
 import DedicationText from "./DedicationText";
 
 const meta: Meta<typeof DedicationText> = {

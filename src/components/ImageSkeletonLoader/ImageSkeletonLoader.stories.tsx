5c8dfc5a47851b941a2878551cd63258da886752
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 14:18:53 -0300

    refactor(ImageSkeletonLoader.stories.tsx): Atualiza pasta raiz do arquivo

--- src/components/ImageSkeletonLoader/ImageSkeletonLoader.stories.tsx
+++ src/components/ImageSkeletonLoader/ImageSkeletonLoader.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { type Meta, type StoryObj } from "@storybook/react";
 import ImageSkeletonLoader from "./ImageSkeletonLoader";
 
 const meta: Meta<typeof ImageSkeletonLoader> = {

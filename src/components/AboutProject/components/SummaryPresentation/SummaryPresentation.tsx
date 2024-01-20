ca5b4158a8c34f892c25fde9f6c06c8d8997575a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 19:38:53 -0300

    update(SummaryPresentation.tsx): Adicionando data-testid ao componente

--- src/components/AboutProject/components/SummaryPresentation/SummaryPresentation.tsx
+++ src/components/AboutProject/components/SummaryPresentation/SummaryPresentation.tsx
@@ -3,7 +3,7 @@ import { SummaryImage, SummaryText } from "./components";
 
 export default function SummaryPresentation(): ReactElement {
   return (
-    <article className="flex flex-col items-center gap-4 lg:flex-row">
+    <article className="flex flex-col items-center gap-4 lg:flex-row" data-testid="SummaryPresentation">
       <SummaryImage />
       <SummaryText />
     </article>

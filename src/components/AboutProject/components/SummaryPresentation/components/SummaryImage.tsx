6a5114a74a7639fc867f5bdb968de10da96a5a7e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 19:38:28 -0300

    update(SummaryImage].tsx): Adicionando data-testid ao componente

--- src/components/AboutProject/components/SummaryPresentation/components/SummaryImage.tsx
+++ src/components/AboutProject/components/SummaryPresentation/components/SummaryImage.tsx
@@ -5,11 +5,14 @@ export default function SummaryImage(): ReactElement {
     <a className="w-2/4 md:w-2/6 lg:w-8/12 rounded-full hover:scale-105 cursor-pointer transition-all delay-75"
       href="https://github.com/bush1D3v"
       target="_blank"
+      rel="noreferrer noopener"
+      data-testid="SummaryImage"
     >
       <img className="rounded-full -mt-4"
         src="https://avatars.githubusercontent.com/u/133554156?v=4"
         alt="Victor photo"
         loading="lazy"
+        data-testid="SummaryImageImage"
       />
     </a>
   );

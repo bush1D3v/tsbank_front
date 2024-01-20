b61aaec8b76f11eb6a35059fa812ef90a935f7c1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 14:28:47 -0300

    feat(SummaryPresentation.tsx): Cria componente para secao de apresentacao da pagina AboutProject

---
+++ src/components/AboutProject/components/SummaryPresentation/SummaryPresentation.tsx
@@ -0,0 +1,11 @@
+import { type ReactElement } from "react";
+import { SummaryImage, SummaryText } from "./components";
+
+export default function SummaryPresentation(): ReactElement {
+  return (
+    <article className="flex flex-col items-center gap-4 lg:flex-row">
+      <SummaryImage />
+      <SummaryText />
+    </article>
+  );
+}

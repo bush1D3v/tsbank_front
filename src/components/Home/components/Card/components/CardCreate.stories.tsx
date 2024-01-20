7e1e0e683c771dfbd9a589e3d5fa552ef14595a0
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-20 14:20:20 -0300

    refactor(CardCreate.stories.tsx): Atualiza pasta raiz do arquivo

--- src/components/Home/components/Card/components/CardCreate.stories.tsx
+++ src/components/Home/components/Card/components/CardCreate.stories.tsx
@@ -1,6 +1,6 @@
 import { withRouter } from "storybook-addon-react-router-v6";
 import CardCreate from "./CardCreate";
-import { Meta } from "@storybook/react";
+import { type Meta } from "@storybook/react";
 import Footer from "../../../../Footer";
 import Header from "../../../../Header";
 

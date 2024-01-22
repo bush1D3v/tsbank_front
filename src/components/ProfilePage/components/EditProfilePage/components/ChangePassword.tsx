462b4c7a67716606c9dd3081d4b4c461724975d3
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-22 20:20:54 -0300

    update(ChangePassword.tsx): Adicionando valores de data-testid

--- src/components/ProfilePage/components/EditProfilePage/components/ChangePassword.tsx
+++ src/components/ProfilePage/components/EditProfilePage/components/ChangePassword.tsx
@@ -52,6 +52,7 @@ export default function ChangePassword(): ReactElement {
   return (
     <S.FormWrapper
       onSubmit={handleSubmit(onSubmit)}
+      data-testid="ChangePassword"
     >
       <Modal
         isOpen={isModalOpen}
@@ -60,7 +61,10 @@ export default function ChangePassword(): ReactElement {
         description={error}
         btnMessage="Try again"
       />
-      <h2 className="font-bold text-2xl lg:text-3xl xl:text-4xl pt-10">
+      <h2
+        className="font-bold text-2xl lg:text-3xl xl:text-4xl pt-10"
+        data-testid="ChangePasswordSubtitle"
+      >
         Change Your Password
       </h2>
       <div className="flex gap-7 py-7 flex-col w-11/12 lg:w-3/4">
@@ -72,6 +76,7 @@ export default function ChangePassword(): ReactElement {
         <S.InputField
           type="text"
           placeholder="New Password"
+          data-testid="ChangePasswordNewPassword"
           {...register("userData.new_password")}
         />
         {errors.userData?.password?.message != null && (
@@ -82,9 +87,10 @@ export default function ChangePassword(): ReactElement {
         <S.InputField
           type="password"
           placeholder="Password"
+          data-testid="ChangePasswordPassword"
           {...register("userData.password")}
         />
-        <S.Button type="submit" disabled={!!isLoading}>
+        <S.Button type="submit" disabled={!!isLoading} data-testid="ChangePasswordButton">
           {isLoading ? "Changing..." : "Change"}
         </S.Button>
       </div>

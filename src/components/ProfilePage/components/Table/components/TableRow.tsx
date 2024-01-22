77a635e3cb2c10a83e0f41d8e2746c51fefe3e00
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-22 20:21:31 -0300

    update(TableRow.tsx): Adicionando valores de data-testid

--- src/components/ProfilePage/components/Table/components/TableRow.tsx
+++ src/components/ProfilePage/components/Table/components/TableRow.tsx
@@ -7,11 +7,14 @@ interface TableRowProps {
 
 export default function TableRow({ label, value }: TableRowProps): ReactElement {
   return (
-    <tr className="flex justify-between rounded-2xl border-2 border-lightGray px-2 gap-2">
-      <td className="p-[2px] font-bold">
+    <tr
+      className="flex justify-between rounded-2xl border-2 border-lightGray px-2 gap-2"
+      data-testid="TableRow"
+    >
+      <td className="p-[2px] font-bold" data-testid="TableRowTableDataLabel">
         {label}
       </td>
-      <td className="p-[2px]">
+      <td className="p-[2px]" data-testid="TableRowTableDataValue">
         {value}
       </td>
     </tr>

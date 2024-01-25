c750b9f0ce6ef586f6c237075f35b45fbfc8e6cf
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-25 18:18:34 -0300

    update(HooksList.tsx): Adiciona data-testid e feature para nao exibir card create caso usuario possua dois cartoes

--- src/components/Home/components/Card/components/CardDetail/components/HooksList.tsx
+++ src/components/Home/components/Card/components/CardDetail/components/HooksList.tsx
@@ -1,8 +1,9 @@
 import { type ReactElement } from "react";
 import { handleLinkClick } from "../../../../../../../functions";
+import { type CardData } from "../../../../../../../types";
 import AnchorLink from "../../../../../../AnchorLink";
 
-export default function HooksList(): ReactElement {
+export default function HooksList({ cardData }: { cardData: CardData[] }): ReactElement {
   const {
     VITE_REACT_APP_CREDIT_PAYMENT,
     VITE_REACT_APP_CARD_UPDATE,
@@ -11,16 +12,24 @@ export default function HooksList(): ReactElement {
   } = import.meta.env;
 
   return (
-    <ul className="flex flex-col lg:flex-row w-10/12 lg:w-full gap-5 md:gap-6 lg:gap-8 animate-fade-down animate-duration-700 animate-ease-in-out">
-      <li className="flex w-full">
-        <AnchorLink
-          buttonBg="bg-saturatedBlue hover:bg-transparent"
-          func={handleLinkClick(VITE_REACT_APP_CARD_CREATE)}
-          param={VITE_REACT_APP_CARD_CREATE}
-          text="CREATE CARD"
-        />
-      </li>
-      <li className="flex w-full">
+    <ul
+      className="flex flex-col lg:flex-row w-10/12 lg:w-full gap-5 md:gap-6 lg:gap-8
+      animate-fade-down animate-duration-700 animate-ease-in-out"
+      data-testid="HooksList"
+    >
+      {(cardData[ 0 ]?.credit || cardData[ 0 ]?.debit) && (cardData[ 1 ]?.credit || cardData[ 1 ]?.debit) ? (
+        <></>
+      ) : (
+        <li className="flex w-full" data-testid="HooksListItemCreate">
+          <AnchorLink
+            buttonBg="bg-saturatedBlue hover:bg-transparent"
+            func={handleLinkClick(VITE_REACT_APP_CARD_CREATE)}
+            param={VITE_REACT_APP_CARD_CREATE}
+            text="CREATE CARD"
+          />
+        </li>
+      )}
+      <li className="flex w-full" data-testid="HooksListItemUpdate">
         <AnchorLink
           buttonBg="bg-saturatedBlue hover:bg-transparent"
           func={handleLinkClick(VITE_REACT_APP_CARD_UPDATE)}
@@ -28,7 +37,7 @@ export default function HooksList(): ReactElement {
           text="UPDATE PASSWORD"
         />
       </li>
-      <li className="flex w-full">
+      <li className="flex w-full" data-testid="HooksListItemTransaction">
         <AnchorLink
           buttonBg="bg-saturatedBlue hover:bg-transparent"
           func={handleLinkClick(VITE_REACT_APP_CARD_TRANSACTION)}
@@ -36,7 +45,7 @@ export default function HooksList(): ReactElement {
           text="CARD TRANSACTION"
         />
       </li>
-      <li className="flex w-full">
+      <li className="flex w-full" data-testid="HooksListItemPayment">
         <AnchorLink
           buttonBg="bg-saturatedBlue hover:bg-transparent"
           func={handleLinkClick(VITE_REACT_APP_CREDIT_PAYMENT)}

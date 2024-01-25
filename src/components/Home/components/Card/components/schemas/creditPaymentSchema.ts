d835b2994b4869b821a18bd32f8742e5931f5618
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-25 18:49:41 -0300

    update(creditPaymentSchema.ts): Atualiza userData para cardData

--- src/components/Home/components/Card/components/schemas/creditPaymentSchema.ts
+++ src/components/Home/components/Card/components/schemas/creditPaymentSchema.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const creditPaymentSchema = z.object({
-  userData: z.object({
+  cardData: z.object({
     value: z.string()
       .min(1, { message: "The value cannot be equal to or less than 0" }),
     password: z.string()
-      .min(8, { message: "Password must have at least 8 characters" })
-      .max(16, { message: "Password limited to 16 characters" }),
+      .min(4, { message: "Password must have at least 4 characters" })
+      .max(6, { message: "Password limited to 6 characters" }),
   }),
 });
 

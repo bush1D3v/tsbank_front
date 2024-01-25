2654483daa4c2b2539678e0b11105edf6cf1163f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-25 18:23:10 -0300

    update(InitialPage.tsx): Componentizando pagina inicial

--- src/components/Home/components/InitialPage/InitialPage.tsx
+++ src/components/Home/components/InitialPage/InitialPage.tsx
@@ -1,5 +1,4 @@
 import Loading from "../../../Loading";
-import { GiMoneyStack } from "react-icons/gi";
 import {
   useState,
   type ReactElement,
@@ -9,7 +8,6 @@ import {
   jsonUserParser,
   balanceFormat,
   errorReplace,
-  createHash,
   balanceStringify,
 } from "../../../../functions";
 import {
@@ -17,8 +15,11 @@ import {
   type HistoryData,
   type HistoryResponseProps
 } from "../functions";
-import { useNavigate } from "react-router-dom";
-import { HooksList } from "./components";
+import {
+  BalanceDivision,
+  HooksList,
+  HistoryList
+} from "./components";
 import { detailUserSubmit } from "./functions";
 
 export default function InitialPage(): ReactElement {
@@ -73,58 +74,38 @@ export default function InitialPage(): ReactElement {
     }
   }, []);
 
-  const navigation = useNavigate();
-
-  const navigate = (to: string) => {
-    navigation(to);
-  };
-
   const { user } = jsonUserParser(sessionStorage.getItem("userData"));
 
   const balance = balanceFormat(user.balance);
 
   return (
-    <section className="flex flex-col items-center justify-center gap-8 md:gap-14 lg:w-7/12" data-testid="InitialPage">
+    <section
+      className="flex flex-col items-center justify-center gap-8 md:gap-14 lg:w-7/12"
+      data-testid="InitialPage"
+    >
       {isLoading ? (
         <Loading />
       ) : (
         <>
           <h1 className="text-xl md:text-2xl lg:text-3l xl:text-4xl font-bold text-center
-          animate-fade-down animate-duration-700 animate-ease-in-out">
+            animate-fade-down animate-duration-700 animate-ease-in-out"
+            data-testid="InitialPageTitle">
             Hello <br /> {user.name}👋
           </h1>
-          <div className="flex gap-4 justify-center w-full animate-fade-down animate-duration-700 animate-ease-in-out">
-            <GiMoneyStack className="w-10 h-10 md:w-12 md:h-12 lg:w-15 lg:h-15" />
-            <p className="text-2xl md:text-3xl lg:text-4xl">{balance}</p>
-          </div>
+          <BalanceDivision balance={balance} />
           <HooksList />
-          <span className="md:-mt-8 -mb-6 md:-mb-12 text-lg md:text-xl lg:text-2xl font-bold
-          animate-fade animate-duration-700 animate-ease-in-out">
+          <span
+            className="md:-mt-8 -mb-6 md:-mb-12 text-lg md:text-xl lg:text-2xl font-bold
+            animate-fade animate-duration-700 animate-ease-in-out"
+            data-testid="InitialPageHistorySpan">
             &#x25BC; HISTORY &#x25BC;
           </span>
           {typeof historyData === "string" ? (
-            <p className="px-4 text-center text-lg md:text-xl lg:text-2xl">{errorReplace(historyData)}</p>
+            <p className="px-4 text-center text-lg md:text-xl lg:text-2xl">
+              {errorReplace(historyData)}
+            </p>
           ) : (
-            <ul className="p-1 flex flex-col gap-4 md:gap-6 lg:gap-4 w-full max-h-[18dvh] md:max-h-[19dvh]
-            overflow-y-scroll lg:w-3/5 xl:w-2/4 animate-fade-down animate-duration-500 animate-ease-in-out">
-              {[ ...historyData ].reverse().map((history, index) => (
-                <li
-                  className="group relative text-center flex gap-2 md:gap-4 justify-center
-                  items-center bg-saturatedBlue rounded-2xl p-4 md:text-xl
-                  lg:text-2xl cursor-pointer hover:bg-darkBlue border-2 border-darkBlue
-                  transition-all delay-75 ease-in-out"
-                  key={index}
-                  onClick={() => navigate(`/transaction/${createHash(history.id.toString())}`)}
-                >
-                  <p className="font-bold capitalize">{history.description}</p>
-                  <p className="text-base md:text-lg lg:text-xl">{history.date}</p>
-                  <span className="absolute pr-2 right-0 text-whiteBlue text-lg md:text-xl
-                  group-hover:text-desaturatedBlue transition-all delay-75 ease-in-out">
-                    &gt;
-                  </span>
-                </li>
-              ))}
-            </ul>
+            <HistoryList historyData={historyData} />
           )}
         </>
       )}

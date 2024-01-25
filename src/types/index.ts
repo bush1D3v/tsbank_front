b7ec14c885024bdbb607c1e570bf1bc7d1227afd
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-25 18:45:31 -0300

    update(index.ts): Atualiza importacao de tipos da aplicacao

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,11 @@
 import { type User } from "./IUser";
 import { type UserData } from "./IUserData";
+import { type TransactionData } from "./ITransactionData";
+import { type CardData } from "./ICardData";
 
 export {
-  User,
-  UserData,
+  type User,
+  type UserData,
+  type TransactionData,
+  type CardData
 };

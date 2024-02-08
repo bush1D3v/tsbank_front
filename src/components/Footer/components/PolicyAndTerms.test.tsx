b31ac8bd0bcfced62026214516b27a106de7ff7c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-08 20:36:09 -0300

    test(PolicyAndTerms.test.tsx): Cria testes para o componente PolicyAndTerms

---
+++ src/components/Footer/components/PolicyAndTerms.test.tsx
@@ -0,0 +1,86 @@
+import "@testing-library/jest-dom";
+import { screen, render } from "@testing-library/react";
+import {
+  describe,
+  test,
+  expect,
+  beforeEach,
+} from "vitest";
+import PolicyAndTerms from "./PolicyAndTerms";
+
+const PolicyAndTermsTestId: string = "PolicyAndTerms";
+const PolicyLinkTestId: string = "PolicyLink";
+const TermsLinkTestId: string = "TermsLink";
+
+describe("PolicyAndTerms component tests", () => {
+  beforeEach(() => {
+    render(<PolicyAndTerms />);
+  });
+
+  test("Should be able to render the component correctly", () => {
+    expect(screen.getByTestId(PolicyAndTermsTestId)).toBeInTheDocument();
+  });
+
+  test("Should be able to render the component with the correctly tailwind classes", () => {
+    expect(screen.getByTestId(PolicyAndTermsTestId)).toHaveClass(`
+    absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]
+    text-center md:flex gap-4
+    `);
+  });
+
+  test("Should be able to render the PolicyLink component with the correctly href", () => {
+    expect(screen.getByTestId(PolicyLinkTestId)).toHaveAttribute(
+      "href", "https://drive.google.com/file/d/1Mjdv1KKwJpRGx7gDOQ_X80mUAUpsSy7j/view?usp=sharing"
+    );
+  });
+
+  test("Should be able to render the PolicyLink component with the correctly target", () => {
+    expect(screen.getByTestId(PolicyLinkTestId)).toHaveAttribute(
+      "target", "_blank"
+    );
+  });
+
+  test("Should be able to render the PolicyLink component with the correctly rel", () => {
+    expect(screen.getByTestId(PolicyLinkTestId)).toHaveAttribute(
+      "rel", "noreferrer noopener"
+    );
+  });
+
+  test("Should be able to render the PolicyLink component with the correctly tailwind classes", () => {
+    expect(screen.getByTestId(PolicyLinkTestId)).toHaveClass(`
+      underline lg:text-xl hover:text-darkBlue transition-all
+    `);
+  });
+
+  test("Should be able to render the PolicyLink component with the correctly text", () => {
+    expect(screen.getByTestId(PolicyLinkTestId)).toHaveTextContent("Privacy Policy");
+  });
+
+  test("Should be able to render the TermsLink component with the correctly href", () => {
+    expect(screen.getByTestId(TermsLinkTestId)).toHaveAttribute(
+      "href", "https://drive.google.com/file/d/1YPhg5eVoAGnskfyPtwkGv9ch0qC7ZVyQ/view?usp=sharing"
+    );
+  });
+
+  test("Should be able to render the TermsLink component with the correctly target", () => {
+    expect(screen.getByTestId(TermsLinkTestId)).toHaveAttribute(
+      "target", "_blank"
+    );
+  });
+
+  test("Should be able to render the TermsLink component with the correctly rel", () => {
+    expect(screen.getByTestId(TermsLinkTestId)).toHaveAttribute(
+      "rel", "noreferrer noopener"
+    );
+  });
+
+  test("Should be able to render the TermsLink component with the correctly tailwind classes", () => {
+    expect(screen.getByTestId(TermsLinkTestId)).toHaveClass(`
+      underline lg:text-xl hover:text-darkBlue transition-all
+    `);
+  });
+
+  test("Should be able to render the TermsLink component with the correctly text", () => {
+    expect(screen.getByTestId(TermsLinkTestId)).toHaveTextContent("Terms of use");
+  });
+});

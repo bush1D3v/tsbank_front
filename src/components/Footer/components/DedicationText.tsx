2da4d1e26f2a95aee70632f576ba6f0d40fd000f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-08 20:34:22 -0300

    update(DedicationText.tsx): Atualizando estilizacao do componente

--- src/components/Footer/components/DedicationText.tsx
+++ src/components/Footer/components/DedicationText.tsx
@@ -2,7 +2,7 @@ import { type ReactElement } from "react";
 
 export default function DedicationText(): ReactElement {
   return (
-    <p className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%] text-center lg:text-xl" data-testid="DedicationText">
+    <p className="text-center lg:text-xl" data-testid="DedicationText">
       Made with 💖 <br /> by&nbsp;
       <a className="font-bold underline cursor-pointer hover:text-darkBlue transition-all"
         href="https://bushi-links.vercel.app/"

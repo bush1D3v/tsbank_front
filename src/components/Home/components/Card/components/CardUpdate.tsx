7ef487208d40b17287007e069ee86dde13194b59
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-12 11:29:52 -0300

    refactor(CardUpdate.tsx): Adiciona module alias a importacao

--- src/components/Home/components/Card/components/CardUpdate.tsx
+++ src/components/Home/components/Card/components/CardUpdate.tsx
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 import { type cardUpdateProps, cardUpdateSchema } from "./schemas";
 import { cardUpdateSubmit } from "./functions";
-import * as S from "../../../../Styleds";
-import Modal from "../../../../Modal";
+import * as S from "@/components/Styleds";
+import Modal from "@/components/Modal";
 
 export default function CardUpdate(): ReactElement {
   const [ error, setError ] = useState<string>("");

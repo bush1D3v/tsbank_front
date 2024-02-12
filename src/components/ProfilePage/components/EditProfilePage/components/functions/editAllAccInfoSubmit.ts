d623babb33ac7797699374af365654131a4ee985
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-12 11:28:16 -0300

    refactor(editAllAccInfoSubmit.ts): Adiciona module alias a importacao

--- src/components/ProfilePage/components/EditProfilePage/components/functions/editAllAccInfoSubmit.ts
+++ src/components/ProfilePage/components/EditProfilePage/components/functions/editAllAccInfoSubmit.ts
@@ -4,7 +4,7 @@ import {
   handleError,
   jsonUserParser,
   sessionStorageStringify
-} from "../../../../../../functions";
+} from "@/functions";
 
 export interface editAllAccInfoUserResponseProps {
   id: number;

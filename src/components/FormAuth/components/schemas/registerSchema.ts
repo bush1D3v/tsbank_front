caf5d0cc25ccb1bab4b18d7320182da1aaeedf64
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-14 14:36:41 -0300

    update(registerSchema.ts): Atualiza esquema de registro

--- src/components/FormAuth/components/schemas/registerSchema.ts
+++ src/components/FormAuth/components/schemas/registerSchema.ts
@@ -3,10 +3,12 @@ import { z } from "zod";
 export const schemaRegisterForm = z.object({
   userData: z.object({
     name: z.string()
-      .min(8, "Please enter your complete name"),
+      .min(8, "Please enter your complete name")
+      .max(75, "Name limited to 75 characters"),
     email: z.string()
       .min(1, { message: "This field has to be filled" })
-      .email("This is not a valid email"),
+      .email("This is not a valid email")
+      .max(75, "Email limited to 75 characters"),
     password: z.string()
       .min(8, { message: "Password must have at least 8 characters" })
       .max(16, { message: "Password limited to 16 characters" }),

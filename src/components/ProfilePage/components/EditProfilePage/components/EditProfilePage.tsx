884f2db13e990e48dff6f9ccf33bee15373ef035
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:11:14 -0300

    refactor(EditProfilePage.tsx): Adicionando path aliases as importacoes

--- src/components/ProfilePage/components/EditProfilePage/components/EditProfilePage.tsx
+++ src/components/ProfilePage/components/EditProfilePage/components/EditProfilePage.tsx
@@ -1,16 +1,15 @@
+import {
+  DELETE_USER,
+  UPDATE_PHONE,
+  UPDATE_EMAIL,
+  UPDATE_PASSWORD,
+  UPDATE_ALL
+} from "@/utils/routerPaths";
 import { type ReactElement } from "react";
 import AnchorLink from "@/components/AnchorLink";
 import { handleLinkClick } from "@/functions";
 
 export default function EditProfilePage(): ReactElement {
-  const {
-    VITE_REACT_APP_DELETE_USER,
-    VITE_REACT_APP_UPDATE_PHONE,
-    VITE_REACT_APP_UPDATE_EMAIL,
-    VITE_REACT_APP_UPDATE_PASSWORD,
-    VITE_REACT_APP_UPDATE_ALL
-  } = import.meta.env;
-
   return (
     <ul
       className="flex max-w-fit h-[82dvh] flex-col items-center justify-center gap-10 md:gap-14 lg:gap-20 p-2"
@@ -18,40 +17,40 @@ export default function EditProfilePage(): ReactElement {
     >
       <li className="flex w-full" data-testid="EditProfilePageListItemUpdateAll">
         <AnchorLink
-          func={handleLinkClick(VITE_REACT_APP_UPDATE_ALL)}
-          param={VITE_REACT_APP_UPDATE_ALL}
+          func={handleLinkClick(UPDATE_ALL)}
+          param={UPDATE_ALL}
           text="UPDATE ALL ACCOUNT INFO"
           buttonBg="bg-saturatedBlue hover:bg-transparent"
         />
       </li>
       <li className="flex w-full" data-testid="EditProfilePageListItemUpdateEmail">
         <AnchorLink
-          func={handleLinkClick(VITE_REACT_APP_UPDATE_EMAIL)}
-          param={VITE_REACT_APP_UPDATE_EMAIL}
+          func={handleLinkClick(UPDATE_EMAIL)}
+          param={UPDATE_EMAIL}
           text="UPDATE YOUR EMAIL"
           buttonBg="bg-saturatedBlue hover:bg-transparent"
         />
       </li>
       <li className="flex w-full" data-testid="EditProfilePageListItemUpdatePassword">
         <AnchorLink
-          func={handleLinkClick(VITE_REACT_APP_UPDATE_PASSWORD)}
-          param={VITE_REACT_APP_UPDATE_PASSWORD}
+          func={handleLinkClick(UPDATE_PASSWORD)}
+          param={UPDATE_PASSWORD}
           text="UPDATE YOUR PASSWORD"
           buttonBg="bg-saturatedBlue hover:bg-transparent"
         />
       </li>
       <li className="flex w-full" data-testid="EditProfilePageListItemUpdatePhone">
         <AnchorLink
-          func={handleLinkClick(VITE_REACT_APP_UPDATE_PHONE)}
-          param={VITE_REACT_APP_UPDATE_PHONE}
+          func={handleLinkClick(UPDATE_PHONE)}
+          param={UPDATE_PHONE}
           text="UPDATE YOUR PHONE"
           buttonBg="bg-saturatedBlue hover:bg-transparent"
         />
       </li>
       <li className="flex w-full" data-testid="EditProfilePageListItemDeleteUser">
         <AnchorLink
-          func={handleLinkClick(VITE_REACT_APP_DELETE_USER)}
-          param={VITE_REACT_APP_DELETE_USER}
+          func={handleLinkClick(DELETE_USER)}
+          param={DELETE_USER}
           text="DELETE ACCOUNT"
           buttonBg="bg-blackBlue hover:bg-error"
         />

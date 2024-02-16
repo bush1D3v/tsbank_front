b0fc6c0aec8002c6002b6c8e78c237cd2bdf8524
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:08:34 -0300

    refactor(Profile.tsx): Adicionando path aliases as importacoes

--- src/components/Header/components/Profile.tsx
+++ src/components/Header/components/Profile.tsx
@@ -1,15 +1,12 @@
 import { type ReactElement } from "react";
 import { CgProfile } from "react-icons/cg";
 import { handleLinkClick } from "@/functions";
+import { PROFILE } from "@/utils/routerPaths";
 
 export default function Profile(): ReactElement {
-  const {
-    VITE_REACT_APP_PROFILE
-  } = import.meta.env;
-
   return (
     <a
-      onClick={handleLinkClick(VITE_REACT_APP_PROFILE)}
+      onClick={handleLinkClick(PROFILE)}
       className="cursor-pointer transition-all delay-75 relative h-full hover:opacity-50"
       data-testid="Profile">
       <CgProfile className="h-full w-full" data-testid="ProfileIcon" />

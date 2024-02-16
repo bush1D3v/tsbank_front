a3619907e5412cc0b6a42b3aa07c0447423241ef
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:07:57 -0300

    refactor(NavLinks.tsx): Adicionando path aliases as importacoes

--- src/components/Header/components/NavLinks.tsx
+++ src/components/Header/components/NavLinks.tsx
@@ -1,28 +1,24 @@
 import { type ReactElement } from "react";
 import AnchorLink from "@/components/AnchorLink";
 import { handleLinkClick } from "@/functions";
+import { HOME, ABOUT } from "@/utils/routerPaths";
 
 export default function NavLinks(): ReactElement {
-  const {
-    VITE_REACT_APP_HOME,
-    VITE_REACT_APP_ABOUT
-  } = import.meta.env;
-
   return (
     <nav data-testid="NavLinks">
       <ul className="flex gap-6 font-bold" data-testid="NavLinksList">
         <li className="flex" data-testid="NavLinksListItemHome">
           <AnchorLink
-            func={handleLinkClick(VITE_REACT_APP_HOME)}
-            param={VITE_REACT_APP_HOME}
+            func={handleLinkClick(HOME)}
+            param={HOME}
             text="HOME"
             buttonBg="bg-blueBase hover:bg-transparent"
           />
         </li>
         <li className="flex" data-testid="NavLinksListItemAbout">
           <AnchorLink
-            func={handleLinkClick(VITE_REACT_APP_ABOUT)}
-            param={VITE_REACT_APP_ABOUT}
+            func={handleLinkClick(ABOUT)}
+            param={ABOUT}
             text="ABOUT"
             buttonBg="bg-blueBase hover:bg-transparent"
           />

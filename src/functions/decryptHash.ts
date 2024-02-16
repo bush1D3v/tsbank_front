a995d291c788b37acd68eecb646fb83f554ab2af
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:00:10 -0300

    refactor(decryptHash.ts): Adicionando path aliases as importacoes

--- src/functions/decryptHash.ts
+++ src/functions/decryptHash.ts
@@ -1,5 +1,5 @@
 import CryptoJS from "crypto-js";
-import key from "../crypto";
+import key from "@/crypto";
 
 export default function decryptHash(id: string) {
   return CryptoJS.AES.decrypt(id, key).toString(CryptoJS.enc.Utf8);

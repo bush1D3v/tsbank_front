79732a32c491232de4b838ccb59ea7cb6e8d651e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:12:00 -0300

    refactor(Profile.tsx): Adicionando path aliases as importacoes

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import { type ReactElement } from "react";
-import { MainProfile } from "../components";
+import { MainProfile } from "@/components";
 
 export default function Profile(): ReactElement {
   return <MainProfile />;

f295241ffd4a1810306d0d96bb2f6ecc9ec5353c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:12:10 -0300

    refactor(privateRoutes.tsx): Adicionando path aliases as importacoes

--- src/helpers/privateRoutes.tsx
+++ src/helpers/privateRoutes.tsx
@@ -1,14 +1,10 @@
 import { type ReactElement, useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-
-import { UserDataContext } from "../contexts";
+import { UserDataContext } from "@/contexts";
+import { LOGIN } from "@/utils/routerPaths";
 
 export default function PrivateRoutes(): ReactElement {
   const { isUserLoggedIn } = useContext(UserDataContext);
 
-  const {
-    VITE_REACT_APP_LOGIN
-  } = import.meta.env;
-
-  return isUserLoggedIn ? <Outlet /> : <Navigate to={VITE_REACT_APP_LOGIN} />;
+  return isUserLoggedIn ? <Outlet /> : <Navigate to={LOGIN} />;
 }

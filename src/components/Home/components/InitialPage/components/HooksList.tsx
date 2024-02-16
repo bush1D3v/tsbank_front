52890ac55812dd3cef4bb08653083ed6ce7944ae
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:09:08 -0300

    refactor(HooksList.tsx): Adicionando path aliases as importacoes

--- src/components/Home/components/InitialPage/components/HooksList.tsx
+++ src/components/Home/components/InitialPage/components/HooksList.tsx
@@ -1,15 +1,14 @@
 import { type ReactElement } from "react";
 import AnchorLink from "@/components/AnchorLink";
 import { handleLinkClick } from "@/functions";
+import {
+  WITHDRAW,
+  DEPOSIT,
+  PIX,
+  CARD
+} from "@/utils/routerPaths";
 
 export default function HooksList(): ReactElement {
-  const {
-    VITE_REACT_APP_WITHDRAW,
-    VITE_REACT_APP_DEPOSIT,
-    VITE_REACT_APP_PIX,
-    VITE_REACT_APP_CARD
-  } = import.meta.env;
-
   return (
     <ul
       className="flex flex-col lg:flex-row gap-5 md:gap-6 lg:gap-8 w-11/12 animate-fade-down animate-duration-700 animate-ease-in-out"
@@ -18,32 +17,32 @@ export default function HooksList(): ReactElement {
       <li className="flex w-full" data-testid="HooksListItemWithdraw">
         <AnchorLink
           buttonBg="bg-saturatedBlue hover:bg-transparent"
-          func={handleLinkClick(VITE_REACT_APP_WITHDRAW)}
-          param={VITE_REACT_APP_WITHDRAW}
+          func={handleLinkClick(WITHDRAW)}
+          param={WITHDRAW}
           text="WITHDRAW"
         />
       </li>
       <li className="flex w-full" data-testid="HooksListItemDeposit">
         <AnchorLink
           buttonBg="bg-saturatedBlue hover:bg-transparent"
-          func={handleLinkClick(VITE_REACT_APP_DEPOSIT)}
-          param={VITE_REACT_APP_DEPOSIT}
+          func={handleLinkClick(DEPOSIT)}
+          param={DEPOSIT}
           text="DEPOSIT"
         />
       </li>
       <li className="flex w-full" data-testid="HooksListItemCard">
         <AnchorLink
           buttonBg="bg-saturatedBlue hover:bg-transparent"
-          func={handleLinkClick(VITE_REACT_APP_CARD)}
-          param={VITE_REACT_APP_CARD}
+          func={handleLinkClick(CARD)}
+          param={CARD}
           text="CARD"
         />
       </li>
       <li className="flex w-full" data-testid="HooksListItemPix">
         <AnchorLink
           buttonBg="bg-saturatedBlue hover:bg-transparent"
-          func={handleLinkClick(VITE_REACT_APP_PIX)}
-          param={VITE_REACT_APP_PIX}
+          func={handleLinkClick(PIX)}
+          param={PIX}
           text="PIX"
         />
       </li>

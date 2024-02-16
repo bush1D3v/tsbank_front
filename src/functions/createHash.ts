7ef9e3f623e722bb3aead9351fd25f6ae27dde12
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:00:17 -0300

    refactor(createHash.ts): Adicionando path aliases as importacoes

--- src/functions/createHash.ts
+++ src/functions/createHash.ts
@@ -1,5 +1,5 @@
 import CryptoJS from "crypto-js";
-import key from "../crypto";
+import key from "@/crypto";
 
 export default function createHash(id: string) {
   let hashValue = CryptoJS.AES.encrypt(id, key).toString();

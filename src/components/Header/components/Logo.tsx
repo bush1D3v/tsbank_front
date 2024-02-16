bbb314c7c22d41c8a014b9735167dfe1a4035a51
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:07:18 -0300

    refactor(Logo.tsx): Adicionando path aliases as importacoes

--- src/components/Header/components/Logo.tsx
+++ src/components/Header/components/Logo.tsx
@@ -1,15 +1,12 @@
 import { type ReactElement } from "react";
 import { logo } from "@/assets/images";
 import { handleLinkClick } from "@/functions";
+import { HOME } from "@/utils/routerPaths";
 
 export default function Logo(): ReactElement {
-  const {
-    VITE_REACT_APP_HOME
-  } = import.meta.env;
-
   return (
     <a
-      onClick={handleLinkClick(VITE_REACT_APP_HOME)}
+      onClick={handleLinkClick(HOME)}
       className="cursor-pointer transition-all delay-75 relative h-full hover:opacity-50"
       data-testid="Logo">
       <img src={logo} alt="tsbank logo" className="h-full w-full" data-testid="LogoImage" />

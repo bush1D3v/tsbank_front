c144393c4204d2bb5fd14b72e630c31bb475c736
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 09:10:00 -0300

    refactor(ProfilePage.tsx): Adicionando path aliases as importacoes

--- src/components/ProfilePage/components/ProfilePage.tsx
+++ src/components/ProfilePage/components/ProfilePage.tsx
@@ -2,13 +2,9 @@ import { type ReactElement } from "react";
 import { handleLinkClick, logoutClick } from "@/functions";
 import AnchorLink from "@/components/AnchorLink";
 import Table from "./Table";
+import { LOGIN, UPDATE } from "@/utils/routerPaths";
 
 export default function ProfilePage(): ReactElement {
-  const {
-    VITE_REACT_APP_LOGIN,
-    VITE_REACT_APP_UPDATE
-  } = import.meta.env;
-
   return (
     <div
       className="flex justify-center items-center h-[82dvh] flex-col gap-10 md:gap-14 lg:gap-20 p-2"
@@ -18,16 +14,16 @@ export default function ProfilePage(): ReactElement {
       <ul className="flex gap-8 w-full px-2" data-testid="ProfilePageList">
         <li className="flex w-full" data-testid="ProfilePageListItemEditProfile">
           <AnchorLink
-            func={handleLinkClick(VITE_REACT_APP_UPDATE)}
-            param={VITE_REACT_APP_UPDATE}
+            func={handleLinkClick(UPDATE)}
+            param={UPDATE}
             text="EDIT PROFILE"
             buttonBg="bg-saturatedBlue hover:bg-transparent"
           />
         </li>
         <li className="flex w-full" data-testid="ProfilePageListItemLogout">
           <AnchorLink
-            func={logoutClick(VITE_REACT_APP_LOGIN)}
-            param={VITE_REACT_APP_LOGIN}
+            func={logoutClick(LOGIN)}
+            param={LOGIN}
             text="LOGOUT"
             buttonBg="bg-blackBlue hover:bg-error"
           />

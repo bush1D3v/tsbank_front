09fd825c4af7214d320d607de97cddcb8cc47422
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-02-16 08:56:17 -0300

    refactor(cardDetailSubmit.ts): Adicionando path aliases as importacoes

--- src/components/Home/components/Card/components/functions/cardDetailSubmit.ts
+++ src/components/Home/components/Card/components/functions/cardDetailSubmit.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-
 import { handleError, jsonUserParser } from "@/functions";
 import { type CardData } from "@/types";
+import { API_BASE_URL, CARD_DETAIL_ENDPOINT } from "@/utils/apiPaths";
 
 export interface CardDetailResponseProps {
   success: boolean;
@@ -11,13 +11,8 @@ export interface CardDetailResponseProps {
 export async function cardDetailSubmit(): Promise<CardDetailResponseProps> {
   const { token } = jsonUserParser(sessionStorage.getItem("userData"));
 
-  const {
-    VITE_REACT_APP_API_BASE_URL,
-    VITE_REACT_APP_CARD_DETAIL_ENDPOINT
-  } = import.meta.env;
-
   try {
-    const response = await axios.get(`${VITE_REACT_APP_API_BASE_URL}${VITE_REACT_APP_CARD_DETAIL_ENDPOINT}`,
+    const response = await axios.get(`${API_BASE_URL}${CARD_DETAIL_ENDPOINT}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,

49a2d53c87694f7f689f196ce0289a44ece92fa2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 07:43:51 -0300

    update(index.ts): Atualiza exportacao de imagens

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,12 +2,16 @@ import lpDevelopmentTeam from "./development-team-lp.png";
 import lpInterface from "./interface-lp.png";
 import lpWelcome from "./welcome-lp.png";
 import lpResponsive from "./responsive-lp.png";
-import logo from "./tsbank-logo.png";
+import smallLogo from "./small-tsbank-logo.png";
+import mediumLogo from "./medium-tsbank-logo.png";
+import largeLogo from "./tsbank-logo.png";
 
 export {
   lpDevelopmentTeam,
   lpInterface,
   lpWelcome,
   lpResponsive,
-  logo
+  smallLogo,
+  mediumLogo,
+  largeLogo
 };

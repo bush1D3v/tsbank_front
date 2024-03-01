2c8b41d5807754eae0f8fc2dc1342569cbbb9549
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 08:17:56 -0300

    update(Profile.tsx): Adiciona aria-label e href

--- src/components/Header/components/Profile.tsx
+++ src/components/Header/components/Profile.tsx
@@ -1,14 +1,14 @@
 import { type ReactElement } from "react";
 import { CgProfile } from "react-icons/cg";
-import { handleLinkClick } from "@/functions";
 import { PROFILE } from "@/utils/routerPaths";
 
 export default function Profile(): ReactElement {
   return (
     <a
-      onClick={handleLinkClick(PROFILE)}
       className="cursor-pointer transition-all delay-75 relative h-full hover:opacity-50"
-      data-testid="Profile">
+      data-testid="Profile"
+      href={PROFILE}
+      aria-label="Profile">
       <CgProfile className="h-full w-full" data-testid="ProfileIcon" />
     </a>
   );

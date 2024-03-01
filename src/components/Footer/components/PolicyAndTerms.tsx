1bea42fa3217a7e592715ce6e19b8f9c5fd297a8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 08:15:03 -0300

    update(PolicyAndTerms.tsx): Atualiza estilizacao para melhor usabilidade

--- src/components/Footer/components/PolicyAndTerms.tsx
+++ src/components/Footer/components/PolicyAndTerms.tsx
@@ -3,28 +3,28 @@ import { ReactElement } from "react";
 export default function PolicyAndTerms(): ReactElement {
   return (
     <ul className="absolute top-2/4 left-2/4 translate-x-[-50%] translate-y-[-50%]
-    text-center md:flex gap-4"
+    text-center md:flex gap-4 md:gap-6 lg:gap-8 flex"
       data-testid="PolicyAndTerms">
       <li>
         <a
           href="https://drive.google.com/file/d/1Mjdv1KKwJpRGx7gDOQ_X80mUAUpsSy7j/view?usp=sharing"
-          className="underline lg:text-xl hover:text-darkBlue transition-all"
+          className="underline text-[18px] md:text-2xl lg:text-3xl hover:text-darkBlue transition-all"
           rel="noreferrer noopener"
           target="_blank"
           data-testid="PolicyLink"
         >
-          Privacy Policy
+          Policy
         </a>
       </li>
       <li>
         <a
           href="https://drive.google.com/file/d/1YPhg5eVoAGnskfyPtwkGv9ch0qC7ZVyQ/view?usp=sharing"
-          className="underline lg:text-xl hover:text-darkBlue transition-all"
+          className="underline text-[18px] md:text-2xl lg:text-3xl hover:text-darkBlue transition-all"
           rel="noreferrer noopener"
           target="_blank"
           data-testid="TermsLink"
         >
-          Terms of use
+          Terms
         </a>
       </li>
     </ul>

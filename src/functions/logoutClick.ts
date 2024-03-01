078959f458803f8f5412fa7afc1556830cd436e4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 08:28:41 -0300

    update(logoutClick.ts): Atualiza funcao de logout

--- src/functions/logoutClick.ts
+++ src/functions/logoutClick.ts
@@ -1,14 +1,3 @@
-import { useNavigate } from "react-router-dom";
-
-export default function logoutClick(to: string) {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const navigation = useNavigate();
-
-  const makeLogout = () => {
-    sessionStorage.clear();
-    navigation(to);
-    window.location.reload();
-  };
-
-  return makeLogout;
+export default function logoutClick() {
+  return sessionStorage.clear();
 }

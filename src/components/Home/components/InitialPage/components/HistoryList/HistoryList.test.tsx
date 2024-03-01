dd0f6e5b3aa8a02c497d5a2c4726082247847c25
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 08:12:54 -0300

    test(HistoryList.test.tsx): Atualiza teste de estilizacao

--- src/components/Home/components/InitialPage/components/HistoryList/HistoryList.test.tsx
+++ src/components/Home/components/InitialPage/components/HistoryList/HistoryList.test.tsx
@@ -59,7 +59,7 @@ describe("HistoryList component tests", () => {
 
   test("Should be able to render the component with the correctly tailwind classes", () => {
     expect(screen.getByTestId(HistoryListTestId)).toHaveClass(
-      "py-1 pl-[18px] pr-1 flex flex-col gap-4 md:gap-6 lg:gap-4 w-full max-h-[18dvh] md:max-h-[19dvh] overflow-y-scroll lg:w-3/5 xl:w-2/4 animate-fade-down animate-duration-500 animate-ease-in-out"
+      "p-1 flex flex-col gap-4 md:gap-6 lg:gap-4 w-full max-h-[18dvh] md:max-h-[19dvh] overflow-y-scroll lg:w-3/5 xl:w-2/4 animate-fade-down animate-duration-500 animate-ease-in-out"
     );
   });
 

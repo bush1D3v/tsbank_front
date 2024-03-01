330547a123ade5c833379b8004f833f1921c2b4f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 08:13:58 -0300

    test(LoadMoreButton.test.tsx): Atualiza testes de onClick

--- src/components/Home/components/InitialPage/components/HistoryList/components/LoadMoreButton.test.tsx
+++ src/components/Home/components/InitialPage/components/HistoryList/components/LoadMoreButton.test.tsx
@@ -14,34 +14,34 @@ import LoadMoreButton from "./LoadMoreButton";
 
 const LoadMoreButtonTestId: string = "LoadMoreButton";
 
-const handleLinkClick: () => void = vi.fn();
+const handleClick = vi.fn();
 
 describe("LoadMoreButton", () => {
   test("should render correctly", () => {
-    render(<LoadMoreButton loading={false} onclick={handleLinkClick} />);
+    render(<LoadMoreButton loading={false} onclick={handleClick} />);
     expect(screen.getByTestId(LoadMoreButtonTestId)).toBeInTheDocument();
   });
 
   test("should render with the correct tailwind classes", () => {
-    render(<LoadMoreButton loading={false} onclick={handleLinkClick} />);
+    render(<LoadMoreButton loading={false} onclick={handleClick} />);
     expect(screen.getByTestId(LoadMoreButtonTestId)).toHaveClass(
       "bg-darkBlue hover:bg-saturatedBlue text-whiteBlue mt-4 rounded-xl p-2 md:p-3 lg:p-4 md:text-xl lg:text-2xl cursor-pointer transition-all delay-75 ease-in-out border-2 border-saturatedBlue"
     );
   });
 
   test("should render with the correct text", () => {
-    render(<LoadMoreButton loading={false} onclick={handleLinkClick} />);
+    render(<LoadMoreButton loading={false} onclick={handleClick} />);
     expect(screen.getByTestId(LoadMoreButtonTestId)).toHaveTextContent("Load More");
   });
 
   test("should render with the correct text when loading", () => {
-    render(<LoadMoreButton loading={true} onclick={() => true} />);
+    render(<LoadMoreButton loading={true} onclick={handleClick} />);
     expect(screen.getByTestId(LoadMoreButtonTestId)).toHaveTextContent("Loading...");
   });
 
   test("should fire event when clicked", () => {
-    render(<LoadMoreButton loading={false} onclick={handleLinkClick} />);
+    render(<LoadMoreButton loading={false} onclick={handleClick} />);
     fireEvent.click(screen.getByTestId(LoadMoreButtonTestId));
-    expect(handleLinkClick).toHaveBeenCalled();
+    expect(handleClick).toHaveBeenCalled();
   });
 });

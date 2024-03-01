4ef723eaa1e22993386039b33f30c02f2b08ae5b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 07:47:35 -0300

    update(SummaryImage.tsx): Adicionando width e height a imagem

--- src/components/AboutProject/components/SummaryPresentation/components/SummaryImage.tsx
+++ src/components/AboutProject/components/SummaryPresentation/components/SummaryImage.tsx
@@ -2,7 +2,7 @@ import { type ReactElement } from "react";
 
 export default function SummaryImage(): ReactElement {
   return (
-    <a className="w-2/4 md:w-2/6 lg:w-8/12 rounded-full hover:scale-105 cursor-pointer transition-all delay-75"
+    <a className="w-2/4 md:w-2/6 lg:w-8/12 hover:scale-105 cursor-pointer transition-all delay-75"
       href="https://github.com/bush1D3v"
       target="_blank"
       rel="noreferrer noopener"
@@ -11,6 +11,8 @@ export default function SummaryImage(): ReactElement {
       <img className="rounded-full -mt-4"
         src="https://avatars.githubusercontent.com/u/133554156?v=4"
         alt="Victor photo"
+        width="100%"
+        height="100%"
         loading="lazy"
         data-testid="SummaryImageImage"
       />

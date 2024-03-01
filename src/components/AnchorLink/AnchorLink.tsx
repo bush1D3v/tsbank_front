28d8fa96391a16608e460477532ecc28c9c4666f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-01 07:50:46 -0300

    update(AnchorLink.tsx): Atualiza funcionalidade do componente

--- src/components/AnchorLink/AnchorLink.tsx
+++ src/components/AnchorLink/AnchorLink.tsx
@@ -1,23 +1,21 @@
 import { type ReactElement } from "react";
-import { eventClick } from "@/functions";
 
-interface AnchorLinkProps {
-  func: (param: string) => void;
+type AnchorLinkProps = {
   text: string;
   param: string;
-  buttonBg: string;
+  buttonBg: `bg-${string} hover:bg-${string}`;
 }
 
-export default function AnchorLink({ func, text, param, buttonBg }: AnchorLinkProps): ReactElement {
+export default function AnchorLink({ text, param, buttonBg }: AnchorLinkProps): ReactElement {
   const className =
     `px-3 md:py-1 lg:px-3 xl:px-4 ${buttonBg} border-white cursor-pointer rounded-xl border-2 font-bold
     hover:scale-110 transition-all delay-75 ease-in-out w-full text-center flex justify-center items-center`;
 
   return (
     <a
-      onClick={eventClick({ func, param })}
       className={className}
-      data-testid="AnchorLink">
+      data-testid="AnchorLink"
+      href={param}>
       {text}
     </a >
   );
